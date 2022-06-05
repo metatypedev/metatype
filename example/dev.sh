a0bc22cfffc72984f0db18b51045c5b112542f4c
Teo Stocco <teo@zifeo.com>
2022-06-05 03:22:34 +0200

    add back submodules

--- example/dev.sh
+++ example/dev.sh
@@ -5,11 +5,9 @@ export PYTHONUNBUFFERED=1
 export RUSTFLAGS="$RUSTFLAGS -A dead_code -A unused_variables"
 export RUST_LOG=resolvers=DEBUG
 
-concurrently --kill-others --names graph,ntgra,gate1,ntgat,cots,cons,gate2 \
+concurrently --kill-others --names graph,ntgra,gate1,ntgat,gate2 \
     "cargo watch --workdir . --ignore example --watch ../typegraph --ignore 'native' --shell 'tg dev'" \
     "cargo watch --workdir ../typegraph/native --shell 'maturin develop'" \
     "cargo watch --workdir ../typegate --ignore native/src --ignore '*.json' --ignore migrations --ignore tests --ignore target --ignore workers --shell 'TG_PORT=7891 ./run.sh'" \
     "cargo watch --workdir ../typegate/native --ignore '*.ts' --ignore '*.json' --shell 'rm -rf ~/Library/Caches/deno/plug/file && deno_bindgen'" \
-    "pnpm -C ../console ts" \
-    "pnpm -C ../console dev" \
     #"cargo watch --workdir ../typegate --ignore native/src --ignore 'native/*.json' --ignore migrations --ignore tests --ignore target --ignore workers --shell 'TG_PORT=7892 ./run.sh > /dev/null'" \
