6b7bfe5c2a9c713581351c8a0ce01bc237542da2
Natoandro <anatoandro@hotmail.com>
2022-06-20 10:55:08 +0300

    Implement body fields and query fields

--- typegate/tests/rest_test.ts
+++ typegate/tests/rest_test.ts
@@ -1,5 +1,5 @@
 import { gql, test } from "./utils.ts";
-import * as mf from "https://deno.land/x/mock_fetch@0.2.0/mod.ts";
+import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
 
 mf.install();
 
@@ -33,11 +33,23 @@ mf.mock("GET@/api/posts", (_req, _match) => {
   });
 });
 
-mf.mock("GET@/api/posts/:id", (_req, match) => {
-  return new Response(JSON.stringify(generatePost(Number(match.params.id))), {
+mf.mock("GET@/api/posts/:id", (_req, params) => {
+  return new Response(JSON.stringify(generatePost(Number(params.id))), {
     status: 200,
   });
 });
+
+// mf.mock("DELETE@/api/posts/:postId", async (req, params) => {
+//   const postId = Number(params.postId);
+//   if (Number.isNaN(postId)) {
+//     return new Response(JSON.stringify({ error: "bad request" }), { status: 400 });
+//   }
+//   const res = JSON.stringify({ postId: Number(params.postId) });
+//   console.log({ res });
+//   return new Response(res, {
+//     status: 200
+//   });
+// });
 
 mf.mock("GET@/api/comments", (req) => {
   const params = new URL(req.url).searchParams;
@@ -51,17 +63,20 @@ mf.mock("GET@/api/comments", (req) => {
 });
 
 // TODO: query params and body
-// mf.mock("POST@/api/comments", async (req) => {
-//   const params = new URL(req.url).searchParams;
-//   const postId = Number(params.get("postId") ?? NaN);
-//   if (Number.isNaN(postId)) {
-//     return new Response("not found", { status: 404 });
-//   }
-//   return new Response(JSON.stringify({
-//     id: NEW_COMMENT_ID,
-//     ...(await req.json())
-//   }));
-// });
+mf.mock("POST@/api/comments", async (req) => {
+  const params = new URL(req.url).searchParams;
+  const postId = Number(params.get("postId") ?? NaN);
+  if (Number.isNaN(postId)) {
+    return new Response(JSON.stringify({ error: "not found" }), {
+      status: 404,
+    });
+  }
+  return new Response(JSON.stringify({
+    id: NEW_COMMENT_ID,
+    postId,
+    ...(await req.json()),
+  }));
+});
 
 test("Rest queries", async (t) => {
   const e = await t.pythonFile("./tests/typegraphs/rest.py");
@@ -127,4 +142,35 @@ test("Rest queries", async (t) => {
       comments: getComments(14),
     }).on(e);
   });
+
+  await t.should("work with query params and body", async () => {
+    await gql`
+      mutation {
+        postComment(postId: 12, content: "Right!") {
+          id
+          postId
+          content
+        }
+      }
+    `.expectData({
+      postComment: {
+        id: NEW_COMMENT_ID,
+        postId: 12,
+        content: "Right!",
+      },
+    }).on(e);
+  });
+
+  // await t.should("work with DELETE method", async () => {
+  //   await gql`
+  //     mutation {
+  //       deletePost(postId: 12) {
+  //         postId
+  //       }
+  //     }
+  //   `.expectData({
+  //     deletePost: {
+  //       postId: 12
+  //     }
+  //   }).on(e);
 });
