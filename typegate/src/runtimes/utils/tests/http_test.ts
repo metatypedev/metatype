93ca8fb79f23a4aca786c6179ebec134d41796a2
Natoandro <anatoandro@hotmail.com>
2022-06-20 17:11:33 +0300

    Refactor http_test.ts: add test steps

--- typegate/src/runtimes/utils/tests/http_test.ts
+++ typegate/src/runtimes/utils/tests/http_test.ts
@@ -1,153 +1,155 @@
 import { assertEquals } from "std/testing/asserts.ts";
 import { getFieldLists, replaceDynamicPathParams } from "../http.ts";
 
-Deno.test("dynamic path params: {param} syntax", () => {
-  assertEquals(
-    replaceDynamicPathParams("/posts/{postId}", { postId: 12, text: "Haha" }),
-    { pathname: "/posts/12", restArgs: { text: "Haha" } },
-  );
+Deno.test("dynamic path params", async (t) => {
+  await t.step("{param} syntax", () => {
+    assertEquals(
+      replaceDynamicPathParams("/posts/{postId}", { postId: 12, text: "Haha" }),
+      { pathname: "/posts/12", restArgs: { text: "Haha" } },
+    );
+    assertEquals(
+      replaceDynamicPathParams("/posts/{postId}/comments/{commentNo}", {
+        postId: 12,
+        commentNo: 3,
+      }),
+      { pathname: "/posts/12/comments/3", restArgs: {} },
+    );
+  });
 
-  assertEquals(
-    replaceDynamicPathParams("/posts/{postId}/comments/{commentNo}", {
-      postId: 12,
-      commentNo: 3,
-    }),
-    { pathname: "/posts/12/comments/3", restArgs: {} },
-  );
-});
+  await t.step(":param syntax", () => {
+    assertEquals(
+      replaceDynamicPathParams("/posts/:postId", { postId: 12, text: "Haha" }),
+      { pathname: "/posts/12", restArgs: { text: "Haha" } },
+    );
+    assertEquals(
+      replaceDynamicPathParams("/posts/:postId/comments/:commentNo", {
+        postId: 12,
+        commentNo: 3,
+      }),
+      { pathname: "/posts/12/comments/3", restArgs: {} },
+    );
+  });
 
-Deno.test("dynamic path params: :param syntax", () => {
-  assertEquals(
-    replaceDynamicPathParams("/posts/:postId", { postId: 12, text: "Haha" }),
-    { pathname: "/posts/12", restArgs: { text: "Haha" } },
-  );
-
-  assertEquals(
-    replaceDynamicPathParams("/posts/:postId/comments/:commentNo", {
-      postId: 12,
-      commentNo: 3,
-    }),
-    { pathname: "/posts/12/comments/3", restArgs: {} },
-  );
-});
-
-Deno.test("dynamic path params: params without values are not replaced", () => {
-  assertEquals(
-    replaceDynamicPathParams("/posts/{postId}/comments/{commentNo}", {
-      postId: 12,
-    }),
-    { pathname: "/posts/12/comments/{commentNo}", restArgs: {} },
-  );
-  assertEquals(
-    replaceDynamicPathParams("/posts/:postId/comments/:commentNo", {
-      postId: 12,
-    }),
-    { pathname: "/posts/12/comments/:commentNo", restArgs: {} },
-  );
+  await t.step("params without values are not replaced", () => {
+    assertEquals(
+      replaceDynamicPathParams("/posts/{postId}/comments/{commentNo}", {
+        postId: 12,
+      }),
+      { pathname: "/posts/12/comments/{commentNo}", restArgs: {} },
+    );
+    assertEquals(
+      replaceDynamicPathParams("/posts/:postId/comments/:commentNo", {
+        postId: 12,
+      }),
+      { pathname: "/posts/12/comments/:commentNo", restArgs: {} },
+    );
+  });
 });
 
 const args = { a: "a", b: "b", c: "c", d: "d" };
 
-Deno.test("field list: GET", () => {
-  assertEquals(
-    getFieldLists("GET", args, {
-      content_type: "application/json",
-      query_fields: null,
-      body_fields: null,
-    }),
-    { query: ["a", "b", "c", "d"], body: [] },
-  );
-
-  assertEquals(
-    getFieldLists(
-      "GET",
-      args,
-      {
+Deno.test("field list", async (t) => {
+  await t.step("GET/DELETE", () => {
+    assertEquals(
+      getFieldLists("GET", args, {
         content_type: "application/json",
         query_fields: null,
-        body_fields: ["c", "d"],
-      },
-    ),
-    { query: ["a", "b"], body: ["c", "d"] },
-  );
-
-  assertEquals(
-    getFieldLists(
-      "GET",
-      args,
-      {
-        content_type: "application/json",
-        query_fields: ["a", "b"],
         body_fields: null,
-      },
-    ),
-    { query: ["a", "b"], body: [] },
-  );
+      }),
+      { query: ["a", "b", "c", "d"], body: [] },
+    );
 
-  assertEquals(
-    getFieldLists(
-      "GET",
-      args,
-      {
-        content_type: "application/json",
-        query_fields: ["a", "b", "e"],
-        body_fields: ["c", "f"],
-      },
-    ),
-    { query: ["a", "b"], body: ["c"] },
-  );
+    assertEquals(
+      getFieldLists(
+        "GET",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: null,
+          body_fields: ["c", "d"],
+        },
+      ),
+      { query: ["a", "b"], body: ["c", "d"] },
+    );
 
-  // TODO: name clash case
-});
+    assertEquals(
+      getFieldLists(
+        "GET",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: ["a", "b"],
+          body_fields: null,
+        },
+      ),
+      { query: ["a", "b"], body: [] },
+    );
 
-Deno.test("field list: POST", () => {
-  assertEquals(
-    getFieldLists("POST", args, {
-      content_type: "application/json",
-      query_fields: null,
-      body_fields: null,
-    }),
-    { query: [], body: ["a", "b", "c", "d"] },
-  );
+    assertEquals(
+      getFieldLists(
+        "GET",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: ["a", "b", "e"],
+          body_fields: ["c", "f"],
+        },
+      ),
+      { query: ["a", "b"], body: ["c"] },
+    );
 
-  assertEquals(
-    getFieldLists(
-      "POST",
-      args,
-      {
+    // TODO: name clash case
+  });
+
+  await t.step("POST/PUT/PATCH", () => {
+    assertEquals(
+      getFieldLists("POST", args, {
         content_type: "application/json",
         query_fields: null,
-        body_fields: ["c", "d"],
-      },
-    ),
-    { query: ["a", "b"], body: ["c", "d"] },
-  );
-
-  assertEquals(
-    getFieldLists(
-      "POST",
-      args,
-      {
-        content_type: "application/json",
-        query_fields: ["a", "b"],
         body_fields: null,
-      },
-    ),
-    { query: ["a", "b"], body: ["c", "d"] },
-  );
+      }),
+      { query: [], body: ["a", "b", "c", "d"] },
+    );
 
-  assertEquals(
-    getFieldLists(
-      "POST",
-      args,
-      {
-        content_type: "application/json",
-        query_fields: ["a", "b", "e"],
-        body_fields: ["c", "f"],
-      },
-    ),
-    { query: ["a", "b"], body: ["c"] },
-  );
+    assertEquals(
+      getFieldLists(
+        "POST",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: null,
+          body_fields: ["c", "d"],
+        },
+      ),
+      { query: ["a", "b"], body: ["c", "d"] },
+    );
 
-  // TODO: name clash case
+    assertEquals(
+      getFieldLists(
+        "POST",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: ["a", "b"],
+          body_fields: null,
+        },
+      ),
+      { query: ["a", "b"], body: ["c", "d"] },
+    );
+
+    assertEquals(
+      getFieldLists(
+        "POST",
+        args,
+        {
+          content_type: "application/json",
+          query_fields: ["a", "b", "e"],
+          body_fields: ["c", "f"],
+        },
+      ),
+      { query: ["a", "b"], body: ["c"] },
+    );
+
+    // TODO: name clash case
+  });
 });
