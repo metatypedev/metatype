8445e9777cea5340a91b5083c1548d5db084cb62
Natoandro <anatoandro@hotmail.com>
2022-06-21 17:57:06 +0300

    Instantiate HTTPRuntime for the REST API

--- example/examples/petstore_v3.py
+++ example/examples/petstore_v3.py
@@ -1,7 +1,8 @@
 from typegraph.graphs.typegraph import TypeGraph
 from typegraph.importers.openapi import import_openapi
+from typegraph.materializers.http import HTTPRuntime
 
 import_openapi("https://petstore3.swagger.io/api/v3/openapi.json", False)
 
 with TypeGraph(name="petstore-v3") as g:
-    pass
+    remote = HTTPRuntime("https://petstore3.swagger.io/api/v3")
