e408872d29331cecad70060179261457f3fe7312
Natoandro <anatoandro@hotmail.com>
2022-06-22 20:16:11 +0300

    Refactor: Add Document class

--- typegraph/typegraph/importers/openapi.py
+++ typegraph/typegraph/importers/openapi.py
@@ -9,139 +9,137 @@ from redbaron import RedBaron
 import semver
 
 
-def typify(schema: Box, name: str = "", opt: bool = False):
-    if opt:
-        return f"t.optional({typify(schema, name)})"
+class Document:
+    def __init__(self, uri):
+        self.uri = uri
+        self.root = Box(httpx.get(uri).json())
 
-    if len(name) > 0:
-        return f'{typify(schema)}.named("{name}")'
+    def typify(self, schema: Box, name: str = "", opt: bool = False):
+        if opt:
+            return f"t.optional({self.typify(schema, name)})"
 
-    if "$ref" in schema:
-        match = re.match(r"^#/components/schemas/(\w+)$", schema["$ref"])
-        if match:
-            return f'g("{match.group(1)}")'
-        else:
-            raise Exception("Uh oh")
+        if len(name) > 0:
+            return f'{self.typify(schema)}.named("{name}")'
 
-    if schema.type == "integer":
-        return "t.integer()"
+        if "$ref" in schema:
+            match = re.match(r"^#/components/schemas/(\w+)$", schema["$ref"])
+            if match:
+                return f'g("{match.group(1)}")'
+            else:
+                raise Exception("Uh oh")
 
-    if schema.type == "number":
-        # formats??
-        return "t.float()"
+        if schema.type == "integer":
+            return "t.integer()"
 
-    if schema.type == "string":
-        # TODO: formats: byte, binary, date, date-time, password
-        return "t.string()"
+        if schema.type == "number":
+            # formats??
+            return "t.float()"
 
-    if schema.type == "boolean":
-        return "t.boolean()"
+        if schema.type == "string":
+            # TODO: formats: byte, binary, date, date-time, password
+            return "t.string()"
 
-    if schema.type == "array":
-        return f't.list({typify(schema["items"])})'
+        if schema.type == "boolean":
+            return "t.boolean()"
 
-    if schema.type == "object":
-        ret = "t.struct({"
+        if schema.type == "array":
+            return f't.list({self.typify(schema["items"])})'
+
+        if schema.type == "object":
+            ret = "t.struct({"
+            required = []
+            if "required" in schema:
+                required = schema.required
+            if "properties" in schema:
+                for prop_name, prop_schema in schema.properties.items():
+                    ret += f'"{prop_name}": {self.typify(prop_schema, opt = not prop_name in required)},'
+            ret += "})"
+            return ret
+
+        raise f'Type "{schema.type}" not supported'
+
+    def gen_schema_defs(self, schemas: Box):
+        cg = ""
+        for name, schema_obj in schemas.items():
+            cg += f"    {self.typify(schema_obj, name)}\n"
+        return cg
+
+    def input_type(self, op: Box):
+        props = {}
         required = []
-        if "required" in schema:
-            required = schema.required
-        if "properties" in schema:
-            for prop_name, prop_schema in schema.properties.items():
-                ret += f'"{prop_name}": {typify(prop_schema, opt = not prop_name in required)},'
-        ret += "})"
-        return ret
+        if "parameters" in op:
+            for param in op.parameters:
+                # TODO: optional???
+                props[param.name] = param.schema
+                if "required" in param and param.required:
+                    required.append(param.name)
+        return self.typify(
+            Box({"type": "object", "properties": props, "required": required})
+        )
 
-    raise f'Type "{schema.type}" not supported'
+    def gen_functions(self, paths: Box):
+        fns = {}
+        for path, item in paths.items():
+            if "$ref" in item:
+                raise "$ref not supported in path definition"
 
+            # TODO: params
 
-def gen_schema_defs(schemas: Box):
-    cg = ""
-    for name, schema_obj in schemas.items():
-        cg += f"    {typify(schema_obj, name)}\n"
-    return cg
+            OPS = ["get", "put", "post", "delete", "options", "head", "patch", "trace"]
+            for op in OPS:
+                if op in item:
+                    op_obj = item[op]
+                    inp = self.input_type(op_obj)
+                    out = "t.struct({})"
+                    has_default = "default" in op_obj.responses
+                    if has_default or "200" in op_obj.responses:
+                        res = op_obj.responses["default" if has_default else "200"]
+                        if "content" in res:
+                            res = res.content
+                            if "application/json" in res:
+                                out = self.typify(res["application/json"].schema)
+                            else:
+                                raise Exception(
+                                    f"Unsupported response types {res.keys()}"
+                                )
+                        else:  # no content
+                            out = "t.optional(t.boolean())"
+                    if "404" in op_obj.responses:
+                        out = f"t.optional({out})"
+                    fns[
+                        op_obj.operationId
+                    ] = f'remote.{op}("{path}", {inp}, {out}).add_policy(allow_all())'
 
+        return fns
 
-OPS = ["get", "put", "post", "delete", "options", "head", "patch", "trace"]
+    def codegen(self):
+        ver = semver.VersionInfo.parse(self.root.openapi)
+        assert ver.major == 3
+        assert ver.minor <= 0
 
+        self.root.info
+        self.root.paths
 
-def input_type(op: Box):
-    props = {}
-    required = []
-    if "parameters" in op:
-        for param in op.parameters:
-            # TODO: optional???
-            props[param.name] = param.schema
-            if "required" in param and param.required:
-                required.append(param.name)
-    return typify(Box({"type": "object", "properties": props, "required": required}))
+        if "servers" in self.root:
+            server_url = self.root.servers[0].url
+        else:
+            server_url = "/"
 
+        url = urljoin(self.uri, server_url)
 
-def gen_functions(paths: Box):
-    fns = {}
-    for path, item in paths.items():
-        if "$ref" in item:
-            raise "$ref not supported in path definition"
-
-        # TODO: params
-
-        for op in OPS:
-            if op in item:
-                op_obj = item[op]
-                inp = input_type(op_obj)
-                out = "t.struct({})"
-                has_default = "default" in op_obj.responses
-                if has_default or "200" in op_obj.responses:
-                    res = op_obj.responses["default" if has_default else "200"]
-                    if "content" in res:
-                        res = res.content
-                        if "application/json" in res:
-                            out = typify(res["application/json"].schema)
-                        else:
-                            raise Exception(f"Unsupported response types {res.keys()}")
-                    else:  # no content
-                        out = "t.optional(t.boolean())"
-                if "404" in op_obj.responses:
-                    out = f"t.optional({out})"
-                fns[
-                    op_obj.operationId
-                ] = f'remote.{op}("{path}", {inp}, {out}).add_policy(allow_all())'
-
-    return fns
+        cg = ""
+        cg += f'    remote = HTTPRuntime("{url}")\n'
+        if "components" in self.root:
+            if "schemas" in self.root.components:
+                cg += self.gen_schema_defs(self.root.components.schemas)
+        cg += f"    g.expose({as_kwargs(self.gen_functions(self.root.paths))})"
+        return cg
 
 
 def as_kwargs(kwargs: dict[str, str]):
     cg = ""
     for key, val in kwargs.items():
         cg += f"{key}={val},"
-    return cg
-
-
-def codegen(uri: str):
-    openapi = Box(httpx.get(uri).json())
-    ver = semver.VersionInfo.parse(openapi.openapi)
-    assert ver.major == 3
-    assert ver.minor <= 0
-
-    openapi.info
-    openapi.paths
-
-    if "servers" in openapi:
-        server_url = openapi.servers[0].url
-    else:
-        server_url = "/"
-
-    url = urljoin(uri, server_url)
-
-    cg = ""
-
-    cg += f'    remote = HTTPRuntime("{url}")\n'
-
-    if "components" in openapi:
-        if "schemas" in openapi.components:
-            cg += gen_schema_defs(openapi.components.schemas)
-
-    cg += f"    g.expose({as_kwargs(gen_functions(openapi.paths))})"
-
     return cg
 
 
@@ -176,7 +174,7 @@ def import_openapi(uri: str, gen: bool):
             code.insert(0, f"from {frm} import {imp}\n")
 
     wth = code.find("with")
-    wth.value = codegen(uri)
+    wth.value = Document(uri).codegen()
 
     new_code = black.format_str(code.dumps(), mode=black.FileMode())
 
