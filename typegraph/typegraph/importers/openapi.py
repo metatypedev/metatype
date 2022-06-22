d34c41065aa38d69e53fb0da75b8987e8a2038b1
Natoandro <anatoandro@hotmail.com>
2022-06-22 12:20:04 +0300

    Define access functions

--- typegraph/typegraph/importers/openapi.py
+++ typegraph/typegraph/importers/openapi.py
@@ -9,7 +9,7 @@ from redbaron import RedBaron
 import semver
 
 
-def typify(schema: dict, name: str = ""):
+def typify(schema: Box, name: str = ""):
     if len(name) > 0:
         return f'{typify(schema)}.named("{name}")'
 
@@ -40,18 +40,71 @@ def typify(schema: dict, name: str = ""):
 
     if schema.type == "object":
         ret = "t.struct({"
-        for prop_name, prop_schema in schema.properties.items():
-            ret += f'"{prop_name}": {typify(prop_schema)},'
+        if "properties" in schema:
+            for prop_name, prop_schema in schema.properties.items():
+                ret += f'"{prop_name}": {typify(prop_schema)},'
         ret += "})"
         return ret
 
     raise f'Type "{schema.type}" not supported'
 
 
-def gen_schema_defs(schemas: dict):
+def gen_schema_defs(schemas: Box):
     cg = ""
     for name, schema_obj in schemas.items():
         cg += f"    {typify(schema_obj, name)}\n"
+    return cg
+
+
+OPS = ["get", "put", "post", "delete", "options", "head", "patch", "trace"]
+
+
+def input_type(op: Box):
+    props = {}
+    if "parameters" in op:
+        for param in op.parameters:
+            # TODO: optional???
+            props[param.name] = param.schema
+    return typify(Box({"type": "object", "properties": props}))
+
+
+def gen_functions(paths: Box):
+    fns = {}
+    for path, item in paths.items():
+        if "$ref" in item:
+            raise "$ref not supported in path definition"
+
+        # TODO: params
+
+        for op in OPS:
+            if op in item:
+                op_obj = item[op]
+                inp = input_type(op_obj)
+                out = "t.struct({})"
+                has_default = "default" in op_obj.responses
+                if has_default or "200" in op_obj.responses:
+                    res = op_obj.responses["default" if has_default else "200"]
+                    if "content" in res:
+                        res = res.content
+                        if "application/json" in res:
+                            out = typify(res["application/json"].schema)
+                        else:
+                            raise Exception(f"Unsupported response types {res.keys()}")
+                    else:  # no content
+                        out = "t.optional(t.boolean())"
+                if "404" in op_obj.responses:
+                    out = f"t.optional({out})"
+                fns[
+                    op_obj.operationId
+                ] = f'remote.{op}("{path}", {inp}, {out}).add_policy(allow_all())'
+
+    return fns
+
+
+def as_kwargs(kwargs: dict[str, str]):
+    cg = ""
+    for key, val in kwargs.items():
+        cg += f"{key}={val},"
     return cg
 
 
@@ -79,6 +132,8 @@ def codegen(uri: str):
         if "schemas" in openapi.components:
             cg += gen_schema_defs(openapi.components.schemas)
 
+    cg += f"    g.expose({as_kwargs(gen_functions(openapi.paths))})"
+
     return cg
 
 
@@ -95,6 +150,7 @@ def import_openapi(uri: str, gen: bool):
         ["typegraph.materializers.http", "HTTPRuntime"],
         ["typegraph.types", "typedefs as t"],
         ["typegraph.graphs.typegraph", "TypeGraph"],
+        ["typegraph.policies", "allow_all"],
     ]
 
     importer = code.find(
