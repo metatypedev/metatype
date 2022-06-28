73018257e7b1f75d11ea612ebb4c89a94c9d195e
Natoandro <anatoandro@hotmail.com>
2022-06-28 12:02:03 +0300

    Test GraphQL runtime

---
+++ typegate/tests/graphql_test.ts
@@ -0,0 +1,61 @@
+import { gql, test } from "./utils.ts";
+import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
+import { buildSchema, graphql } from "https://cdn.skypack.dev/graphql";
+
+const schema = buildSchema(`
+  type User {
+    id: Int
+    name: String
+    email: String
+  }
+  type Query {
+    user(id: Int): User
+  }
+`);
+
+function generateUser(id: number) {
+  return {
+    id,
+    email: `user.${id}@example.com`,
+    name: `User ${id}`,
+  };
+}
+
+const rootValue = {
+  user: ({ id }: { id: number }) => generateUser(id),
+};
+
+mf.install();
+
+mf.mock("POST@/api/graphql", async (req) => {
+  const { query } = await req.json();
+  const res = await graphql({
+    schema,
+    source: query,
+    rootValue,
+  });
+  return new Response(JSON.stringify(res), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
+test("GraphQL queries", async (t) => {
+  const e = await t.pythonFile("./tests/typegraphs/graphql.py");
+
+  await t.should("work with queries", async () => {
+    await gql`
+      query {
+        user(id: 12) {
+          id
+          name
+          email
+        }
+      }
+    `.expectData({
+      user: generateUser(12),
+    }).on(e);
+  });
+});
