1814198c4a25cde7a697dbd3c8aaaadc7d8de0a2
Teo Stocco <teo@zifeo.com>
2022-08-01 03:10:50 +0200

    start design

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Feature, { FeatureItem } from "../components/Feature";
 
 import styles from "./index.module.css";
 
@@ -24,6 +26,84 @@ function HomepageHeader() {
         </div>
       </div>
     </header>
+  );
+}
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Compose API blocks",
+    Svg: require("@site/static/img/compose-api-blocks.svg").default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: "Design and discover",
+    Svg: require("@site/static/img/design-discover.svg").default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: "Iterate quickly",
+    Svg: require("@site/static/img/iterate-quickly.svg").default,
+    description: (
+      <>
+        Focus on what matters. Extend or customize your website layout by
+        reusing React. Docusaurus can be extended while reusing the same header
+        and footer.
+      </>
+    ),
+  },
+  {
+    title: "Bring your own components",
+    Svg: require("@site/static/img/bring-your-own-components.svg").default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: "Secure by default",
+    Svg: require("@site/static/img/secure-by-default.svg").default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: "Commercial support",
+    Svg: require("@site/static/img/commercial-support.svg").default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
