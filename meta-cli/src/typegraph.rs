4225ee7b0a524b0f643121d68e101f6c42da2f0e
Natoandro <anatoandro@hotmail.com>
2022-08-16 17:58:33 +0200

    (codegen/deno): list exported functions from file

---
+++ meta-cli/src/typegraph.rs
@@ -0,0 +1,49 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Typegraph {
+    pub types: Vec<TypeNode>,
+    pub materializers: Vec<Materializer>,
+    pub runtimes: Vec<TGRuntime>,
+    pub policies: Vec<Policy>,
+    pub codes: Vec<Code>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TypeNode {
+    pub name: String,
+    pub typedef: String,
+    pub edges: Vec<u32>,
+    pub policies: Vec<u32>,
+    pub runtime: u32,
+    pub data: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Materializer {
+    pub name: String,
+    pub runtime: u32,
+    pub data: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TGRuntime {
+    pub name: String,
+    pub data: HashMap<String, Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Policy {
+    pub name: String,
+    pub materializer: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Code {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub typ: String,
+    pub source: String,
+}
