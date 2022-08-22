99fec6007a276c75a145243f495b5da0547fb585
Teo Stocco <teo@zifeo.com>
2022-08-22 05:17:27 +0200

    cleanup + fix push

--- typegate/native/bindings/bindings.ts
+++ typegate/native/bindings/bindings.ts
@@ -1,14 +1,17 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.1/plug.ts"
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.2/plug.ts"
+
 function encode(v: string | Uint8Array): Uint8Array {
   if (typeof v !== "string") return v
   return new TextEncoder().encode(v)
 }
+
 function decode(v: Uint8Array): string {
   return new TextDecoder().decode(v)
 }
+
 function readPointer(v: any): Uint8Array {
-  const ptr = new Deno.UnsafePointerView(v as Deno.UnsafePointer)
+  const ptr = new Deno.UnsafePointerView(v as bigint)
   const lengthBe = new Uint8Array(4)
   const view = new DataView(lengthBe.buffer)
   ptr.copyInto(lengthBe, 0)
@@ -16,9 +19,30 @@ function readPointer(v: any): Uint8Array {
   ptr.copyInto(buf, 4)
   return buf
 }
+
+const url = new URL("../../../target/debug", import.meta.url)
+let uri = url.toString()
+if (!uri.endsWith("/")) uri += "/"
+
+let darwin: string | { aarch64: string; x86_64: string } = uri
+  + "libnative.dylib"
+
+if (url.protocol !== "file:") {
+  // Assume that remote assets follow naming scheme
+  // for each macOS artifact.
+  darwin = {
+    aarch64: uri + "libnative_arm64.dylib",
+    x86_64: uri + "libnative.dylib",
+  }
+}
+
 const opts = {
   name: "native",
-  url: (new URL("../target/debug", import.meta.url)).toString(),
+  urls: {
+    darwin,
+    windows: uri + "native.dll",
+    linux: uri + "libnative.so",
+  },
   policy: CachePolicy.NONE,
 }
 const _lib = await prepare(opts, {
@@ -44,24 +68,11 @@ const _lib = await prepare(opts, {
     nonblocking: true,
   },
 })
-export type PrismaUnregisterEngineOut = {
-  key: string
-}
-export type PrismaRegisterEngineOut = {
-  engine_id: string
-}
-export type PrismaIntrospectionOut = {
-  introspection: string
-}
 export type PrismaIntrospectionInp = {
   datamodel: string
 }
-export type PrismaRegisterEngineInp = {
-  datamodel: string
-  typegraph: string
-}
-export type PrismaUnregisterEngineInp = {
-  key: string
+export type PrismaIntrospectionOut = {
+  introspection: string
 }
 export type PrismaQueryInp = {
   key: string
@@ -70,6 +81,19 @@ export type PrismaQueryInp = {
 }
 export type PrismaQueryOut = {
   res: string
+}
+export type PrismaRegisterEngineInp = {
+  datamodel: string
+  typegraph: string
+}
+export type PrismaRegisterEngineOut = {
+  engine_id: string
+}
+export type PrismaUnregisterEngineInp = {
+  key: string
+}
+export type PrismaUnregisterEngineOut = {
+  key: string
 }
 export function init() {
   let rawResult = _lib.symbols.init()
