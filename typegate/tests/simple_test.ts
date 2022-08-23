684a47cf612a2e294aded69878de0fd805f71cb0
Natoandro <anatoandro@hotmail.com>
2022-08-23 23:03:16 +0200

    Add test cases for variables

--- typegate/tests/simple_test.ts
+++ typegate/tests/simple_test.ts
@@ -18,4 +18,63 @@ test("Simple graph", async (t) => {
       })
       .on(e);
   });
+
+  await t.should("work with root variables", async () => {
+    await gql`
+      query q($val: Int) {
+        test(a: $val) {
+          a
+        }
+      }
+    `
+      .withVars({
+        val: 2,
+      })
+      .expectData({
+        test: {
+          a: 2,
+        },
+      })
+      .on(e);
+  });
+
+  await t.should("reject missing variable", async () => {
+    await gql`
+      query q($val: Int) {
+        rec(nested: { arg: $val })
+      }
+    `
+      .expectErrorContains("missing variable")
+      .on(e);
+  });
+
+  await t.should("accepted variable", async () => {
+    await gql`
+      query q($val: Fake) {
+        rec(nested: $val)
+      }
+    `
+      .withVars({
+        val: { nested: 2 },
+      })
+      .expectData({
+        rec: 2,
+      })
+      .on(e);
+  });
+
+  await t.should("accepted nested variable", async () => {
+    await gql`
+      query q($val: Integer) {
+        rec(nested: { arg: $val })
+      }
+    `
+      .withVars({
+        val: 2,
+      })
+      .expectData({
+        rec: 2,
+      })
+      .on(e);
+  });
 });
--- typegate/tests/typegraphs/simple.py
+++ typegate/tests/typegraphs/simple.py
@@ -1,16 +1,26 @@
 from typegraph import policies
 from typegraph.graphs.typegraph import TypeGraph
 from typegraph.materializers import deno
+from typegraph.materializers import worker
 from typegraph.types import typedefs as t
 
 with TypeGraph("testing") as g:
 
+    rec = t.func(
+        t.struct({"nested": t.struct({"arg": t.integer()})}),
+        t.integer(),
+        worker.JavascriptMat(
+            g.fun(worker.JavascriptMat.lift(lambda args: args["nested"]["arg"])),
+        ),
+    )
+
     g.expose(
+        rec=rec.add_policy(policies.allow_all()),
         test=t.func(
             t.struct({"a": t.integer().named("arg1")}).named("inp"),
             t.struct({"a": t.integer().named("deps")}).named("res"),
             deno.FunMat("identity"),
         )
         .named("f")
-        .add_policy(policies.allow_all())
+        .add_policy(policies.allow_all()),
     )
