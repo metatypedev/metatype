efdf82d2daf6874136ffb6d5a0f8e5864d6fa69b
Natoandro <anatoandro@hotmail.com>
2022-08-24 08:52:16 +0200

    Enable previously disabled tests

--- typegate/tests/vars_test.ts
+++ typegate/tests/vars_test.ts
@@ -3,7 +3,7 @@ import { gql, test } from "./utils.ts";
 test("GraphQL variables", async (t) => {
   const e = await t.pythonFile("./tests/typegraphs/vars.py");
 
-  await t.should("work with query", async () => {
+  await t.should("work with root vars", async () => {
     await gql`
       query Add($first: Int!, $second: Int!) {
         add(first: $first, second: $second)
@@ -18,23 +18,6 @@ test("GraphQL variables", async (t) => {
       })
       .on(e);
 
-    // This does not work:
-    // https://stackoverflow.com/questions/63041747
-    //
-    //   await gql`
-    //     query Sum($first: Int!, $second: Int!) {
-    //       sum(numbers: [2, 3])
-    //     }
-    //   `
-    //     .withVars({
-    //       first: 2,
-    //       second: 3,
-    //     })
-    //     .expectData({
-    //       sum: 5,
-    //     })
-    //     .on(e);
-
     await gql`
       query Sum($numbers: [Int]!) {
         sum(numbers: $numbers)
@@ -44,6 +27,22 @@ test("GraphQL variables", async (t) => {
     }).expectData({
       sum: 10,
     })
+      .on(e);
+  });
+
+  await t.should("work with nested vars in array", async () => {
+    await gql`
+        query Sum($first: Int!, $second: Int!) {
+          sum(numbers: [$first, $second])
+        }
+      `
+      .withVars({
+        first: 2,
+        second: 3,
+      })
+      .expectData({
+        sum: 5,
+      })
       .on(e);
   });
 });
