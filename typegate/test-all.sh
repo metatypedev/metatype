b4d01d5a8de0f79ef706b3dccb946d4f3c956389
Natoandro <anatoandro@hotmail.com>
2022-08-24 23:34:27 +0200

    Update and comment typegate/test-all.sh

--- typegate/test-all.sh
+++ typegate/test-all.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-clear
+# This is a temporary alternative to `./test.sh --quiet`
+# which currently triggers a segmentation fault, until the issue is solved
+# https://github.com/denoland/deno_bindgen/issues/79
 
-for test in `ls tests/*_test.ts`; do
+for test in `find . -name '*_test.ts'`; do
   ./test.sh --quiet $test
 done
