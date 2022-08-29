f9a733b9d5be9651c4c10a4f7b832ad444579ae9
Teo Stocco <teo@zifeo.com>
2022-08-29 02:14:32 +0200

    fix docker

--- libs/common/src/lib.rs
+++ libs/common/src/lib.rs
@@ -2,7 +2,7 @@ pub fn get_version() -> String {
     let version = env!("CARGO_PKG_VERSION").to_string();
     if cfg!(debug_assertions) {
         let commit = git_version::git_version!();
-        format!("{version}+{commit}2222")
+        format!("{version}+{commit}")
     } else {
         version
     }
