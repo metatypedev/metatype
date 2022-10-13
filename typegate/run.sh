427ba36e4c39551b519f9f3ce3a9d04a6957d8c4
Teo Stocco <teo@zifeo.com>
2022-10-12 21:37:16 -0500

    fix tests

--- typegate/run.sh
+++ typegate/run.sh
@@ -4,9 +4,9 @@ set -e
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
+# --lock=lock.json \ https://github.com/denoland/deno/issues/16120
 cd ${SCRIPT_PATH} && \
     exec deno run \
-    # --lock=lock.json \ https://github.com/denoland/deno/issues/16120
     --import-map=import_map.json \
     --unstable \
     --allow-run=hostname \
