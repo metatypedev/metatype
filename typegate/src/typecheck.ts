c5c34aac3ef00de681eee7046ba7a5bdfee5bb5e
Natoandro <anatoandro@hotmail.com>
2022-10-21 11:51:22 +0200

    Typechecking queries and responses

--- typegate/src/typecheck.ts
+++ typegate/src/typecheck.ts
@@ -1,7 +1,7 @@
 // Copyright Metatype under the Elastic License 2.0.
 
 // deno-lint-ignore-file no-unused-vars
-import { TSchema, Type } from "typebox";
+import { ObjectOptions, TSchema, Type } from "typebox";
 import { Format } from "typebox/format";
 import { TypeCheck as TboxTypeCheck, TypeCompiler } from "typebox/compiler";
 import { Value } from "typebox/value";
@@ -10,6 +10,13 @@ import type * as jst from "json_schema_typed";
 import { ensure, has } from "./utils.ts";
 import { mapValues } from "std/collections/map_values.ts";
 import { updateIn } from "https://deno.land/x/immutable@4.0.0-rc.14-deno/mod.ts";
+import { Kind } from "graphql";
+import {
+  OperationDefinitionNode,
+  SelectionNode,
+  SelectionSetNode,
+} from "graphql/ast";
+import { FragmentDefs } from "./graphql.ts";
 
 // we will use this jsonschema jit compiler: https://github.com/sinclairzx81/typebox
 // and the types format will become a superset of the jsonschema https://json-schema.org/understanding-json-schema/reference/index.html
@@ -51,7 +58,7 @@ const SchemaOptionsType = Type.Object({
   description: Type.Optional(Type.String()),
   default: Type.Optional(Type.Any()),
   examples: Type.Optional(Type.Any()),
-});
+}, { additionalProperties: false });
 
 export class ValidationSchemaBuilder {
   private refs: Set<string> = new Set();
@@ -129,13 +136,147 @@ export class ValidationSchemaBuilder {
 }
 
 export class TypeCheck {
-  private schemasByRef: Map<string, TSchema> = new Map();
-  private schemas: TSchema[] = [];
+  constructor(private baseSchema: JSONSchema) {}
+
+  /**
+   * Ensures that a GraphQL query described by the `operation` and `fragments`
+   * params is valid.
+   * @param operation operation definition node of the GraphQL query to validate
+   * @param fragments fragment definitions for the GraphQL query to validate
+   * @returns Query response typechecking object
+   * @throws {Error} - Thrown if the query is not valid.
+   * The error message describes why the query is not valid.
+   */
+  validateQuery(
+    operation: OperationDefinitionNode,
+    fragments: FragmentDefs,
+  ): QueryTypeCheck {
+    const schema = new QuerySchemaBuilder(this.baseSchema, operation, fragments)
+      .build();
+    return new QueryTypeCheck(schema);
+  }
+}
+
+type Properties = Record<string, JSONSchema>;
+
+class QuerySchemaBuilder {
+  constructor(
+    private baseSchema: JSONSchema,
+    private operation: OperationDefinitionNode,
+    private fragments: FragmentDefs,
+  ) {}
+
+  public build(): JSONSchema {
+    const rootPath = this.operation.name?.value ??
+      (this.operation.operation[0].toUpperCase());
+    return this.get(rootPath, this.baseSchema, this.operation.selectionSet);
+  }
+
+  private resolve(path: string[]): JSONSchema {
+    return path.reduce((o, key, idx) => {
+      const next = o[key];
+      if (next == null) {
+        throw new Error(
+          `schema not found at ${path.join("/")}, value at ${
+            path.slice(0, idx + 1)
+          } is undefined`,
+        );
+      }
+      return next;
+    }, this.baseSchema as any);
+  }
+
+  private get(
+    path: string,
+    base: JSONSchema,
+    selectionSet: SelectionSetNode | undefined,
+  ): JSONSchema {
+    if (has(base, "$ref")) {
+      return this.get(path, this.resolve(refToPath(base.$ref)), selectionSet);
+    }
+
+    switch (base.type) {
+      case "object": {
+        const properties = {} as Properties;
+        const baseProperties = (base.properties ?? {}) as Properties;
+        if (selectionSet == undefined) {
+          throw new Error(`Path ${path} must be a field selection`);
+        }
+
+        const addProperty = (node: SelectionNode) => {
+          switch (node.kind) {
+            case Kind.FIELD: {
+              const { name, selectionSet } = node;
+              if (has(baseProperties, name.value)) {
+                properties[name.value] = this.get(
+                  `${path}.${name.value}`,
+                  baseProperties[name.value],
+                  selectionSet,
+                );
+              } else {
+                throw new Error(`${path}.${name.value} is undefined`);
+              }
+              break;
+            }
+
+            case Kind.FRAGMENT_SPREAD: {
+              const fragment = this.fragments[node.name.value];
+              for (const selectionNode of fragment.selectionSet.selections) {
+                addProperty(selectionNode);
+              }
+              break;
+            }
+
+            case Kind.INLINE_FRAGMENT: {
+              for (const selectionNode of node.selectionSet.selections) {
+                addProperty(selectionNode);
+              }
+              break;
+            }
+          }
+        };
+
+        for (const node of selectionSet.selections) {
+          addProperty(node);
+        }
+
+        const schema = {
+          ...base,
+          properties,
+          required: (base.required ?? []).filter((key) => has(properties, key)),
+        };
+        delete schema.$defs;
+
+        return schema;
+      }
+
+      case "array": {
+        return {
+          ...base,
+          items: this.get(path, base.items as JSONSchema, selectionSet),
+        };
+      }
+
+      default:
+        if (selectionSet != undefined) {
+          throw new Error(`Path ${path} cannot be a field selection`);
+        }
+        return base;
+    }
+  }
+}
+
+export class QueryTypeCheck {
+  private typecheckSchema: TSchema;
+  private valueCastSchema: TSchema;
   private typecheck: TboxTypeCheck<any>;
 
-  constructor(private schema: Exclude<jst.JSONSchema, boolean>) {
-    const tschema = this.get();
-    this.typecheck = TypeCompiler.Compile(tschema, this.schemas);
+  constructor(private schema: JSONSchema) {
+    this.typecheckSchema = this.tschema(this.schema, {});
+    this.typecheck = TypeCompiler.Compile(this.typecheckSchema);
+    this.valueCastSchema = this.tschema(this.schema, {
+      additionalProperties: false,
+    });
   }
 
   // We won't be checking any object against the full schema.
@@ -144,37 +285,19 @@ export class TypeCheck {
     return this.typecheck.Check(value);
   }
 
-  public validate(value: any) {
+  public validate(value: any): any {
     if (!this.check(value)) {
-      const errors = [...this.typecheck.Errors(value)];
-      throw new Error(`errors: ${JSON.stringify(errors)}`);
-    }
-  }
-
-  private get(ref?: string): TSchema {
-    if (ref == null) return this.tschema(this.schema);
-    const cached = this.schemasByRef.get(ref);
-    if (cached != null) {
-      return cached;
+      const errors = [...this.typecheck.Errors(value)]
+        .map((err) => `${err.message} at ${err.path}`);
+      throw new Error(`errors: ${errors.join(", ")}`);
     }
 
-    const schema = this.resolve(refToPath(ref));
-
-    const tschema = this.tschema(schema);
-    this.schemasByRef.set(ref, tschema);
-    this.schemas.push(tschema);
-
-    return tschema;
+    return Value.Cast(this.valueCastSchema, value);
   }
 
-  private tschema(schema: JSONSchema): TSchema {
+  private tschema(schema: JSONSchema, objectOptions: ObjectOptions): TSchema {
     if (has(schema, "$ref")) {
-      // TODO handle circular references
-      const ref = this.get(schema.$ref);
-      if (ref.$id == null) {
-        ref.$id = schema.$ref.replace(/[\/#]/g, "_");
-      }
-      return Type.Ref(this.get(schema.$ref));
+      return this.tschema(this.resolve(refToPath(schema.$ref)), objectOptions);
     }
 
     switch (schema.type) {
@@ -197,17 +320,20 @@ export class TypeCheck {
         return Type.Null();
       case "array":
         return Type.Array(
-          this.tschema(schema.items! as JSONSchema),
+          this.tschema(schema.items! as JSONSchema, objectOptions),
           Value.Cast(SchemaOptionsType, schema),
         );
       case "object": {
         const required = new Set(schema.required);
         const props = {} as Record<string, TSchema>;
         for (const [k, s] of Object.entries(schema.properties ?? {})) {
-          const tb = this.tschema(s as JSONSchema);
+          const tb = this.tschema(s as JSONSchema, objectOptions);
           props[k] = required.has(k) ? tb : Type.Optional(tb);
         }
-        return Type.Object(props, Value.Cast(SchemaOptionsType, schema));
+        return Type.Object(props, {
+          ...Value.Cast(SchemaOptionsType, schema),
+          ...objectOptions,
+        });
       }
 
       default:
--- typegate/tests/typecheck_test.ts
+++ typegate/tests/typecheck_test.ts
@@ -1,53 +1,226 @@
 // Copyright Metatype under the Elastic License 2.0.
 
 import tg from "./typegraphs/typecheck.json" assert { type: "json" };
-import { assertThrows } from "std/testing/asserts.ts";
-import { TypeCheck, ValidationSchemaBuilder } from "../src/typecheck.ts";
+import { assert, assertEquals, assertThrows } from "std/testing/asserts.ts";
+import {
+  QueryTypeCheck,
+  TypeCheck,
+  ValidationSchemaBuilder,
+} from "../src/typecheck.ts";
+import { findOperation } from "../src/graphql.ts";
+import { parse } from "graphql";
 
-Deno.test("test typecheck", () => {
-  const b = new ValidationSchemaBuilder(tg.schema);
-  const typecheck = new TypeCheck(b.build());
+Deno.test("typecheck", async (t) => {
+  const validationSchema = new ValidationSchemaBuilder(tg.schema).build();
+
+  const typecheck = new TypeCheck(validationSchema);
+
+  const gql = (strings: readonly string[], ...args: any[]) => {
+    const query = strings
+      .map((q, i) => `${q}${args[i] ? JSON.stringify(args[i]) : ""}`)
+      .join("");
+    const [operation, fragments] = findOperation(parse(query), undefined);
+    if (operation == null) {
+      throw new Error("No operation found in the query");
+    }
+    return typecheck.validateQuery(operation, fragments);
+  };
+
+  await t.step("invalid queries", () => {
+    assertThrows(
+      () =>
+        gql`
+        query Q {
+          postis {
+            id
+          }
+        }
+      `,
+      Error,
+      "Q.postis is undefined",
+    );
+
+    assertThrows(
+      () =>
+        gql`
+        query Q {
+          posts {
+            id
+            title
+            text
+          }
+        }
+      `,
+      Error,
+      "Q.posts.text is undefined",
+    );
+  });
+
+  let query1: QueryTypeCheck;
+
+  await t.step("valid query", () => {
+    query1 = gql`
+      query GetPosts {
+        posts {
+          id
+          title
+          author {
+            id
+            name
+          }
+        }
+      }
+    `;
+
+    assert(query1 instanceof QueryTypeCheck);
+  });
+
   const post1 = {
     id: crypto.randomUUID(),
     title: "Hello",
     content: "Hello, World!",
   };
 
-  assertThrows(
-    () => typecheck.validate({ posts: [post1] }),
-    Error,
-    "/posts/0/author",
-  );
+  const user1 = { id: crypto.randomUUID() };
+  const user2 = { ...user1, name: "John" };
 
-  const user = {
-    id: crypto.randomUUID(),
-    name: "John",
-    email: "john@example.com",
-    // website: "https://john.example.com",
-  };
+  const post2 = { ...post1, author: user1 };
+  const post3 = { ...post1, author: user2 };
 
-  const post2 = {
-    ...post1,
-    author: user,
-  };
+  await t.step("data type validation", () => {
+    assertThrows(
+      () => query1.validate({ posts: [post1] }),
+      Error,
+      "Expected object at /posts/0/author",
+    );
 
-  assertThrows(
-    () => typecheck.validate({ posts: [post2] }),
-    Error,
-    "/posts/0/published",
-  );
+    assertThrows(
+      () => query1.validate({ posts: [post2] }),
+      Error,
+      "Expected string at /posts/0/author/name",
+    );
 
-  const post3 = { ...post2, published: false };
-  typecheck.validate({ posts: [post3] });
+    query1.validate({ posts: [post3] });
 
-  delete (user as any).website;
-  typecheck.validate({ posts: [post3] });
+    assertThrows(
+      () => query1.validate({ posts: [post3, post2] }),
+      Error,
+      "Expected string at /posts/1/author/name",
+    );
 
-  delete (user as any).email;
-  typecheck.validate(post3);
-  assertThrows(
-    () => typecheck.validate({ posts: [post3] }),
-    Error,
-    "/posts/0/author/email",
-  );
+    const query2 = gql`
+      query GetPosts {
+        posts {
+          id
+          title
+          author {
+            id
+            name
+            email
+            website
+          }
+        }
+      }
+    `;
+
+    assertThrows(
+      () =>
+        query2.validate({
+          posts: [
+            {
+              ...post1,
+              author: {
+                id: "user-id",
+              },
+            },
+          ],
+        }),
+      Error,
+      "Expected string to match format 'uuid' at /posts/0/author/id",
+    );
+
+    assertThrows(
+      () =>
+        query2.validate({
+          posts: [{
+            ...post1,
+            author: {
+              id: crypto.randomUUID(),
+              name: "John",
+              email: "my email address",
+            },
+          }],
+        }),
+      Error,
+      "Expected string to match format 'email' at /posts/0/author/email",
+    );
+
+    assertThrows(
+      () =>
+        query2.validate({
+          posts: [{
+            ...post1,
+            author: {
+              id: crypto.randomUUID(),
+              name: "John",
+              email: "user@example.com",
+              website: "example.com",
+            },
+          }],
+        }),
+      Error,
+      "Expected string to match format 'uri' at /posts/0/author/website",
+    );
+
+    query2.validate({
+      posts: [{
+        ...post1,
+        author: {
+          id: crypto.randomUUID(),
+          name: "John",
+          email: "user@example.com",
+        },
+      }],
+    });
+
+    query2.validate({
+      posts: [{
+        ...post1,
+        author: {
+          id: crypto.randomUUID(),
+          name: "John",
+          email: "user@example.com",
+          website: "https://example.com",
+        },
+      }],
+    });
+  });
+
+  await t.step("remove extra props in objects", () => {
+    const postId = crypto.randomUUID();
+    const userId = crypto.randomUUID();
+    const value = query1.validate({
+      posts: [{
+        id: postId,
+        title: "Hello",
+        published: false,
+        author: {
+          id: userId,
+          name: "John",
+          email: "user@example.com",
+        },
+      }],
+    });
+
+    assertEquals(value, {
+      posts: [{
+        id: postId,
+        title: "Hello",
+        author: {
+          id: userId,
+          name: "John",
+        },
+      }],
+    });
+  });
 });
