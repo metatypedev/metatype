bf8d306786b73472790344ba8ae63f27a52fb6ef
Teo Stocco <teo@zifeo.com>
2023-01-15 18:28:07 +0100

    fix: tests

--- typegraph/typegraph/__init__.py
+++ typegraph/typegraph/__init__.py
@@ -1,5 +1,7 @@
 # Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
 
 # shortcut imports
+from typegraph import types as t  # noqa
+from typegraph.graph.typegraph import TypeGraph  # noqa
 
 version = "0.0.3-dev.1"
