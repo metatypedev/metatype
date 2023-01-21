62d0151e09e4c0e3df684637127a4118f5d5fa3d
Teo Stocco <teo@zifeo.com>
2023-01-21 23:09:15 +0100

    manage runtime dependency

---
+++ dev/env.ts
@@ -0,0 +1,66 @@
+import { expandGlobSync } from "https://deno.land/std@0.170.0/fs/expand_glob.ts";
+import * as path from "https://deno.land/std@0.170.0/path/mod.ts";
+
+/**
+ * Wrapper around docker compose to manage runtime dependencies.
+ *
+ * Usage: deno run -A env.ts [<dependency1>... <dependencyN>]
+ *
+ * <dependency>:
+ *    name of the docker-compose file / runtime dependency
+ */
+
+const dcs = Array.from(
+  expandGlobSync(new URL("envs/docker-compose.*.yml", import.meta.url), {
+    includeDirs: false,
+  }),
+);
+const envs = Object.fromEntries(
+  dcs
+    .map((e) => [path.basename(e.path).split(".")[1], e.path]),
+);
+
+const on = new Set([envs["base"]]);
+if (Deno.args.length === 1 && Deno.args[0] === "all") {
+  Object.values(envs).forEach(on.add);
+} else {
+  for (const arg of Deno.args) {
+    if (!envs[arg]) {
+      console.log(
+        `Unknown env "${arg}", available: ${
+          Object.keys(envs).join(", ")
+        } or "all".`,
+      );
+      Deno.exit(1);
+    }
+    on.add(envs[arg]);
+  }
+}
+const off = new Set(Object.values(envs).filter((e) => !on.has(e)));
+
+if (off.size > 0) {
+  await run([
+    "docker-compose",
+    ...Array.from(off).flatMap((f) => ["-f", f]),
+    "down",
+  ]);
+}
+
+if (on.size > 0) {
+  await run([
+    "docker-compose",
+    ...Array.from(on).flatMap((f) => ["-f", f]),
+    "up",
+    "-d",
+  ]);
+}
+
+async function run(cmd: string[]) {
+  const p = Deno.run({
+    cmd,
+    stdout: "piped",
+  });
+  const stdout = await p.output();
+  p.close();
+  return new TextDecoder().decode(stdout).trim();
+}
