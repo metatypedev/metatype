565d9125162efe45a1a3a5432efb7437f20dd680
Teo Stocco <teo@zifeo.com>
2023-01-23 23:41:01 +0100

    fix crash

--- typegate/native/src/lib.rs
+++ typegate/native/src/lib.rs
@@ -1,42 +1,39 @@
 // Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
 
-mod conf;
+mod config;
 mod runtimes;
 mod typegraph;
 
-use crate::conf::CONFIG;
+use envconfig::Envconfig;
 use lazy_static::lazy_static;
 use log::info;
 use macros::deno;
-use static_init::dynamic;
 use std::{borrow::Cow, panic};
 use tokio::runtime::Runtime;
 
 lazy_static! {
+    pub static ref CONFIG: config::Config = config::Config::init_from_env().unwrap();
     static ref RT: Runtime = {
         info!("Runtime created");
         Runtime::new().unwrap()
     };
+    static ref SENTRY: sentry::ClientInitGuard = {
+        sentry::init((
+            CONFIG.sentry_dsn.clone(),
+            sentry::ClientOptions {
+                release: Some(Cow::from(common::get_version())),
+                environment: Some(Cow::from(if CONFIG.debug {
+                    "development".to_string()
+                } else {
+                    "production".to_string()
+                })),
+                sample_rate: CONFIG.sentry_sample_rate,
+                traces_sample_rate: CONFIG.sentry_traces_sample_rate,
+                ..Default::default()
+            },
+        ))
+    };
 }
-
-#[dynamic]
-#[allow(dead_code)]
-static SENTRY: sentry::ClientInitGuard = {
-    sentry::init((
-        CONFIG.sentry_dsn.clone(),
-        sentry::ClientOptions {
-            release: Some(Cow::from(common::get_version())),
-            environment: Some(Cow::from(if CONFIG.debug {
-                "development".to_string()
-            } else {
-                "production".to_string()
-            })),
-            sample_rate: CONFIG.sentry_sample_rate,
-            traces_sample_rate: CONFIG.sentry_traces_sample_rate,
-            ..Default::default()
-        },
-    ))
-};
 
 #[deno]
 fn init() {
