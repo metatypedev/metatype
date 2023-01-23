58e0f7b48133d72ee37fc00f09d1ddccc5c0fe77
Michaël <afmichael73@gmail.com>
2023-01-23 16:07:35 +0300

    replace orjson to json

--- typegraph/typegraph/utils/sanitizers.py
+++ typegraph/typegraph/utils/sanitizers.py
@@ -1,7 +1,7 @@
 # Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
 
-import orjson
+from json import dumps
 
 
 def sanitize_ts_string(content: str):
-    return orjson.dumps(content).decode("utf-8")[1:-1]
+    return dumps(content)[1:-1]
