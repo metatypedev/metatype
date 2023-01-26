1e739f4a4fd899673c5d88af8f64f9ffb4ea614b
zohary <zoharyandrianome@gmail.com>
2023-01-26 14:59:47 +0300

    Using GraphiQL children to customize playground

--- typegate/src/web/playground.ts
+++ typegate/src/web/playground.ts
@@ -8,16 +8,8 @@ export const renderPlayground = (
   documentationUrl?: string,
   typegraphList?: { name: string, url: string }[]
 ) => {
-  const strOptions = (typegraphList || []).map(element => `
-    const option = document.createElement("option");
-    option.value = ${element.url}/${element.name}
-    
-    const tgLink = document.createElement("a");
-    tgLink.href = ${element.url}/${element.name}
-    tgLink.innerHTML = element.name
-    option.append(tgLink)
-
-    ddlTypegraphs.add(option);
+  const strOptions = (typegraphList || []).map(tg => `
+    <option key={${tg.url}} value={${tg.url}}>{${tg.name}}</option>)
   `);
 
   return `
@@ -34,6 +26,9 @@ export const renderPlayground = (
           #graphiql {
             height: 100vh;
           }
+          .graphiql-container .graphiql-session-header {
+            z-index: 1;
+          }
           .graphiql-container .graphiql-ddl-typegraphs {
             margin: auto 15px;
             padding: 0 15px;
@@ -44,8 +39,14 @@ export const renderPlayground = (
             cursor: pointer;
           }
           .graphiql-container .graphiql-logo {
+            display: flex;
+            align-items: center;
             padding: 0 !important;
             margin: 0 15px 0 0;
+          }
+          .graphiql-container .graphiql-logo a{
+            text-decoration: none;
+            color: inherit;
           }
           .graphiql-container .graphiql-logo .graphiql-logo-link {
             width: 40px;
@@ -59,70 +60,39 @@ export const renderPlayground = (
             height: 100%;
             object-fit: contain;
           }
-          .graphiql-container .graphiql-logo p {
-            margin: 0;
+          .graphiql-container .graphiql-logo .graphiql-logo-version {
             font-size: 11px;
           }
           .graphiql-container .graphiql-session-header-right {
             margin-top: 20px;
           }
         </style>
-        <script
-          crossorigin
-          src="https://unpkg.com/react@17/umd/react.development.js"
-        ></script>
-        <script
-          crossorigin
-          src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"
-        ></script>
+        <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
+        <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
         <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
       </head>
       <body>
         <div id="graphiql">Loading...</div>
-        <script
-          src="https://unpkg.com/graphiql/graphiql.min.js"
-          type="application/javascript"
-        ></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/domready/1.0.8/ready.min.js"></script>
-        <script>
-          ReactDOM.render(
-            React.createElement(GraphiQL, {
-              fetcher: GraphiQL.createFetcher({
-                url: '${url}',
-                headers: ${JSON.stringify(headers)}
-              }),
-              defaultEditorToolsVisibility: true,
-            }),
-            document.getElementById('graphiql'),
-          );
-        </script>
-        <script>
-          domready(function () {
-            const logoLink = document.querySelector('.graphiql-logo-link')
-            logoLink.href = '${documentationUrl} || ""'
-            logoLink.innerHTML = '<img src="${logoUrl || "https://graphql.org/img/logo.svg"}" alt="logo" class="logo" />'
+        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+        <script src="https://unpkg.com/graphiql/graphiql.min.js" type="application/javascript"></script>
+        <script data-plugins="transform-es2015-modules-umd" type="text/babel">
+          const fetcher =  GraphiQL.createFetcher({
+            url: ${url}
+          });
+          const logoLink = ${logoUrl || 'https://graphql.org/img/logo.svg'}
+          const documentationLink = ${documentationUrl || 'https://github.com/graphql/graphiql'}
 
-            const logoContainer = document.querySelector('.graphiql-logo');
-            const p = document.createElement('p');
-            p.innerHTML = '${version} || ""';
-            logoContainer.appendChild(p);
-
-            const ddlTypegraphs = document.createElement("select");
-            ddlTypegraphs.classList.add("graphiql-ddl-typegraphs")
-            ddlTypegraphs.onchange = function (event) { 
-              window.location = event.target.value
-            };
-            
-            const defaultOption = document.createElement("option");
-            defaultOption.value = "file:///home/zohary/dev/test/graphiql.html"
-            defaultOption.text = "Select a typegraph"
-            ddlTypegraphs.add(defaultOption);
-
-            ${strOptions}
-
-            const headerRight = document.querySelector('.graphiql-session-header-right');
-            headerRight.insertBefore(ddlTypegraphs, logoContainer)
-          })
+          ReactDOM.render(<GraphiQL fetcher={fetcher}>
+            <GraphiQL.Logo>
+              <select className="graphiql-ddl-typegraphs" onChange={(e => window.location = e.target.value)}>
+                ${strOptions}
+              </select>
+              <a href={documentationLink} target="_blank" rel="noreferrer">
+                <span className="graphiql-logo-link"><img src={logoLink} alt="logo" className="logo" /></span>
+                <span className="graphiql-logo-version">${version || ''}</span>
+              </a>
+            </GraphiQL.Logo>
+          </GraphiQL>, document.getElementById('graphiql'));
         </script>
       </body>
     </html>
