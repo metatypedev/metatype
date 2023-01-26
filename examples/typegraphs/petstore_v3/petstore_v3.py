700128abbe1f061837808bacff62198d309d630d
Natoandro <anatoandro@hotmail.com>
2023-01-26 09:18:24 +0100

    Fix pre commit

--- examples/typegraphs/petstore_v3/petstore_v3.py
+++ examples/typegraphs/petstore_v3/petstore_v3.py
@@ -1,8 +1,5 @@
-from typegraph import t
 from typegraph import TypeGraph
 from typegraph.importers.openapi import OpenApiImporter
-from typegraph.policies import public
-from typegraph.runtimes.http import HTTPRuntime
 
 # Needs support for 'application/octet-stream' request body
 OpenApiImporter("petstore", url="https://petstore3.swagger.io/api/v3/openapi.json").imp(
