daa59fb1c36e8626ae92cd82d56b1f579a98e381
Teo Stocco <zifeo@users.noreply.github.com>
2023-03-05 14:58:47 +0100

    Revert "MET-67 graph implement full prisma mapping (#111)" (#175)
    This reverts commit fa0eefdb7450369d84970d7861321903be35ae30.

--- typegraph/typegraph/types.py
+++ typegraph/typegraph/types.py
@@ -504,18 +504,10 @@ class struct(typedef):
     def additional(self, t: Union[bool, TypeNode]):
         return self.replace(additional_props=t)
 
-    # creates a duplicate type with no runtime
-    def detach(self):
-        return self.replace(
-            name=f"{self.type}_{self.graph.next_type_id()}", runtime=None
-        )
-
     def compose(self, props: Dict[str, typedef]):
         new_props = dict(self.props)
         new_props.update(props)
-        return self.replace(
-            props=frozendict(new_props), name=f"{self.type}_{self.graph.next_type_id()}"
-        )
+        return self.replace(props=frozendict(new_props))
 
     def __getattr__(self, attr):
         try:
