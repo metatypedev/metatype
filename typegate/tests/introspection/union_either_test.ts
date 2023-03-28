c9a23ffdaa45c965920af08beb87e2b8a237c5ea
afmika <afmichael73@gmail.com>
2023-03-28 18:39:23 +0300

    init tests

---
+++ typegate/tests/introspection/union_either_test.ts
@@ -0,0 +1,34 @@
+// Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
+
+import { gql, test } from "../utils.ts";
+
+// https://github.com/graphql/graphql-js/blob/main/src/__tests__/starWarsIntrospection-test.ts
+
+test("Basic introspection", async (t) => {
+  const e = await t.pythonFile("introspection/union_either.py");
+
+  await t.should("allow querying the schema for query type", async () => {
+    await gql`
+      query IntrospectionQuery {
+        __schema {
+          types {
+            name
+            kind
+            possibleTypes {
+              name
+              kind
+              possibleTypes {
+                name
+                kind
+              }
+            }
+          }
+        }
+      }
+    `
+      .expectBody((body: any) => {
+        console.log(body.data.__schema.types);
+      })
+      .on(e);
+  });
+}, { introspection: true });
