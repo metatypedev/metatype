149c96858f1b69eb8cefaabf976d27fae745316c
afmika <afmichael73@gmail.com>
2023-03-31 18:39:29 +0300

    add snapshot/test

---
+++ typegraph/tests/importers/test_google_importer.py
@@ -0,0 +1,19 @@
+# Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
+
+from os import path
+
+from typegraph.importers.base.importer import Codegen
+from typegraph.importers.google_discovery import GoogleDiscoveryImporter
+
+
+def test_codegen(snapshot):
+    importer = GoogleDiscoveryImporter(
+        "test",
+        url="https://example.com/api",
+        file=path.join(path.dirname(__file__), "fcm_googleapi_spec.json"),
+    )
+
+    snapshot.snapshot_dir = "tests/__snapshots__/importers"
+    snapshot.assert_match(
+        importer.codegen(Codegen()).res, "google_importer_codegen.txt"
+    )
