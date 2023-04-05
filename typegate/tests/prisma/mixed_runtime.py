fe0055fda924812b2b3bdbe02d7921a6545f9ed5
Michaël <afmichael73@gmail.com>
2023-04-05 23:13:34 +0300

    MET-143: support multi-runtime in all runtimes (#258)
    * fix output generator
    
    * mix with random runtime

--- typegate/tests/prisma/mixed_runtime.py
+++ typegate/tests/prisma/mixed_runtime.py
@@ -1,26 +1,35 @@
 from typegraph import TypeGraph, effects, policies, t
 from typegraph.providers.prisma.runtimes.prisma import PrismaRuntime
 from typegraph.runtimes.graphql import GraphQLRuntime
+from typegraph.runtimes.random import RandomMat, RandomRuntime
 
 with TypeGraph("prisma") as g:
     db = PrismaRuntime("prisma", "POSTGRES")
-    gql = GraphQLRuntime("https://graphqlzero.almansi.me/api")
+    gql_1 = GraphQLRuntime("https://graphqlzero.almansi.me/api")
+    rand = RandomRuntime(seed=1)
 
     public = policies.public()
-    country = t.struct(
+    post = t.struct(
         {
-            "code": t.string(),
-            "name": t.integer(),
+            "id": t.string(),
+            "title": t.string(),
         },
-    ).named("Country")
+    ).named("Post")
+    user = t.struct(
+        {
+            "name": t.string().config(gen="name"),
+            "age": t.integer().config(gen="age", type="adult"),
+        }
+    ).named("Album")
     record = t.struct(
         {
             "id": t.integer().config("id", "auto"),
             "description": t.string(),
-            "country": gql.query(
-                t.struct({"code": t.string()}),
-                t.optional(country),
+            "post": gql_1.query(
+                t.struct({"id": t.string()}),
+                t.optional(post),
             ),
+            "user": t.gen(user, RandomMat(runtime=rand)),
         },
     ).named("Record")
 
