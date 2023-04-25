65c081a91f3f7d40c01a79ea743265c5d13f5308
afmika <afmichael73@gmail.com>
2023-04-25 19:27:46 +0300

    extend term for nested

--- typegraph/typegraph/providers/prisma/type_generator.py
+++ typegraph/typegraph/providers/prisma/type_generator.py
@@ -134,7 +134,7 @@ class TypeGenerator:
 
     # Example:
     # t.X() => t.either([t.X(), struct_equals, struct_startsWith, ...])
-    def extend_terminal_nodes_props(self, tpe: t.struct) -> t.struct:
+    def extend_terminal_nodes_props(self, tpe: t.struct, skip_rel=False) -> t.struct:
         any_type = self.gen_any_type()
 
         # node level
@@ -160,6 +160,19 @@ class TypeGenerator:
 
         node_props = {}
         for k, v in tpe.props.items():
+            # nested
+            v = resolve_proxy(v)
+            if v.runtime is not None and v.runtime != tpe.runtime:
+                continue
+            nested = resolve_proxy(resolve_entity_quantifier(v))
+            if nested.type == "object":
+                if skip_rel:
+                    continue
+                node_props[k] = self.extend_terminal_nodes_props(
+                    nested, skip_rel=False
+                ).optional()
+                continue
+            # same depth
             term = t.either(term_list + [undo_optional(v)])
             not_node = rename_with_idx(t.struct({"not": term}), "not")
             node_props[k] = t.either([not_node, term]).optional()
@@ -186,9 +199,9 @@ class TypeGenerator:
     # where: { NOT: { NOT: { name: { startsWith: "P" }} }}
     # where: { field1: 4, field2: {gt: 3}}
     def gen_query_where_expr(
-        self, tpe: t.struct, exclude_extra_fields=False
+        self, tpe: t.struct, exclude_extra_fields=False, skip_rel=False
     ) -> t.struct:
-        tpe = self.get_where_type(tpe)
+        tpe = self.get_where_type(tpe, skip_rel)
         extended_tpe = self.extend_terminal_nodes_props(tpe)
         extended_tpe = rename_with_idx(extended_tpe, "extended_tpe")
 
