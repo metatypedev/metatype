a932dd45969e8cdb951a4d36d36cb2756f3b97bd
msga-mmm <michael.sga.mmm@gmail.com>
2023-05-22 16:20:38 -0500

    test(typegate): run gripmock server for test

--- typegate/tests/runtimes/grpc/grpc_test.ts
+++ typegate/tests/runtimes/grpc/grpc_test.ts
@@ -6,6 +6,20 @@ test("Grpc runtime", async (t) => {
   const e = await t.pythonFile("runtimes/grpc/grpc.py");
 
   await t.should("works", async () => {
+    new Deno.Command(
+      "./typegate/tests/runtimes/grpc/grpc_server/start.sh",
+      {
+        stdout: "null",
+      },
+    ).spawn();
+
+    const sleep = new Deno.Command("/bin/sleep", {
+      args: ["5"],
+      stdout: "null",
+    }).spawn();
+
+    await sleep.status;
+
     await gql`
       query {
         greet(name: "Metatype")
@@ -15,5 +29,14 @@ test("Grpc runtime", async (t) => {
         greet: "Hello Metatype",
       })
       .on(e);
+
+    const end = new Deno.Command(
+      "./typegate/tests/runtimes/grpc/grpc_server/shutdown.sh",
+      {
+        stdout: "null",
+      },
+    ).spawn();
+
+    await end.status;
   });
 });
