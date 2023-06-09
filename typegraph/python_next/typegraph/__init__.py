5bb96be2249f03736baf2fa4bdd5679b016589e9
Natoandro <anatoandro@hotmail.com>
2023-06-09 09:57:14 +0300

    refactor: rename typegraph_v2 to typegraph_next

--- typegraph/python_next/typegraph/__init__.py
+++ typegraph/python_next/typegraph/__init__.py
@@ -1,6 +1,6 @@
 # Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
 # SPDX-License-Identifier: MPL-2.0
 
-from typegraph.graph.typegraph import TypeGraph
+from typegraph.graph.typegraph import TypeGraph  # noqa
 
 version = "0.1.9-dev.0"
