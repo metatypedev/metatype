06b94ee8cb7757ee67482f06dc6a701eb5c39e9c
Michaël <afmichael73@gmail.com>
2023-07-28 12:57:19 +0300

    fix: move logic to async_messenger and revert 'feat: add timer'

--- typegate/src/runtimes/deno/deno_messenger.ts
+++ typegate/src/runtimes/deno/deno_messenger.ts
@@ -6,64 +6,10 @@ import { envSharedWithWorkers } from "../../log.ts";
 import { Task } from "./shared_types.ts";
 import { dirname, fromFileUrl, resolve, toFileUrl } from "std/path/mod.ts";
 import { LazyAsyncMessenger } from "../patterns/messenger/lazy_async_messenger.ts";
-import { Message } from "../patterns/messenger/types.ts";
-
-import { BinaryHeap } from "std/collections/binary_heap.ts";
 
 const localDir = dirname(fromFileUrl(import.meta.url));
 const workerFile = toFileUrl(resolve(localDir, "./worker.ts"));
-
-export interface TickCallback {
-  (): Promise<void>;
-}
-
-export class PulseHandle {
-  static createdCount = 0;
-  #stop = false;
-  constructor(
-    private tickSec = 1,
-  ) {
-    PulseHandle.createdCount++;
-  }
-
-  // raw setInterval tends to trigger Leaking async ops errors in tests
-  // the idea is to wrap the timer logic inside promises
-
-  static async sleep(seconds = 1) {
-    // BUG:
-    // not working either
-    // "A child process stdout (rid {RID}) was opened before the test started,
-    // but was closed during the test. Do not close resources
-    // in a test that were not created during that test.""
-
-    // setInterval/setTimeout/worker with setTimeout => leaking ops
-    // external sleep source => leaking ressources
-
-    const cmd = new Deno.Command("sleep", { args: [`${seconds}s`] });
-    await cmd.output();
-    console.log("tick");
-  }
-
-  stop() {
-    this.#stop = true;
-  }
-
-  async loop(fn: TickCallback) {
-    while (!this.#stop) {
-      await Promise.resolve(fn());
-      await PulseHandle.sleep(this.tickSec);
-    }
-    PulseHandle.createdCount--;
-    console.log("stopped safely, remaining ", PulseHandle.createdCount);
-  }
-}
-
 export class DenoMessenger extends LazyAsyncMessenger<Worker, Task, unknown> {
-  pendingOperations: BinaryHeap<
-    { date: number; worker: Worker; message: Message<Task> }
-  > = new BinaryHeap((a, b) => a.date - b.date);
-  doneIds: Set<number> = new Set<number>();
-
   constructor(
     name: string,
     permissions: Deno.PermissionOptionsObject,
@@ -91,10 +37,8 @@ export class DenoMessenger extends LazyAsyncMessenger<Worker, Task, unknown> {
             },
           },
         } as WorkerOptions);
-        worker.onmessage = async (event) => {
-          await receive(event.data);
-          this.doneIds.add(event.data.id);
-          console.log("Removing id", event.data.id);
+        worker.onmessage = (event) => {
+          receive(event.data);
         };
         worker.onerror = (error) => {
           console.error(error.message);
@@ -114,65 +58,19 @@ export class DenoMessenger extends LazyAsyncMessenger<Worker, Task, unknown> {
       },
       ops,
       (broker, message) => {
-        this.pendingOperations.push({
-          date: Date.now(),
-          worker: broker,
-          message: message,
-        });
         broker.postMessage(message);
       },
       (broker) => {
         broker.terminate();
       },
     );
-
-    const tickMs = 1;
-    const maxDurationMs = 5000;
-
-    const pulse = new PulseHandle(tickMs);
-    pulse.loop(async () => {
-      const currentDate = Date.now();
-      const item = this.pendingOperations.peek(); // O(1)
-      if (item !== undefined) {
-        const delta = currentDate - item.date;
-        if (this.doneIds.has(item.message.id)) {
-          this.pendingOperations.pop(); // O(log N)
-          this.doneIds.delete(item.message.id); // O(1)
-
-          // console.log(
-          //   "done safely",
-          //   item.message.id,
-          //   "currentSize",
-          //   this.pendingOperations.length,
-          // );
-
-          if (this.pendingOperations.length == 0) {
-            pulse.stop();
-          }
-        } else if (delta >= maxDurationMs) {
-          this.receive({
-            id: item.message.id,
-            error: `timeout exceeded for id ${item.message.id}`,
-          });
-
-          this.pendingOperations.pop(); // O(log N)
-          await this.terminate();
-
-          // console.log(
-          //   "Removing id",
-          //   item.message.id,
-          //   "op",
-          //   item.message.op,
-          //   "currentSize",
-          //   this.pendingOperations.length,
-          // );
-
-          if (this.pendingOperations.length == 0) {
-            pulse.stop();
-          }
-        }
-      }
-    });
+    // this.pendingOperations.push({
+    //   date: Date.now(),
+    //   worker: broker,
+    //   message: message,
+    // });
+    // this.doneIds.add(event.data.id);
+    // console.log("Removing id", event.data.id);
 
     if (lazy) {
       this.enableLazyness();
--- typegate/src/runtimes/patterns/messenger/async_messenger.ts
+++ typegate/src/runtimes/patterns/messenger/async_messenger.ts
@@ -5,6 +5,7 @@ import { deferred } from "std/async/deferred.ts";
 import { getLogger } from "../../../log.ts";
 import { Answer, Message, TaskData } from "./types.ts";
 import { maxi32 } from "../../../utils.ts";
+import { BinaryHeap } from "std/collections/binary_heap.ts";
 
 const logger = getLogger(import.meta);
 
@@ -26,6 +27,12 @@ export class AsyncMessenger<Broker, M, A> {
   #send: MessengerSend<Broker, M>;
   #stop: MessengerStop<Broker>;
 
+  #timer: ReturnType<typeof setInterval>;
+  #pendingOperations: BinaryHeap<
+    { date: number; message: Message<M> }
+  > = new BinaryHeap((a, b) => a.date - b.date); // min date always at top
+  #doneIds: Set<number> = new Set<number>();
+
   protected constructor(
     broker: Broker,
     send: MessengerSend<Broker, M>,
@@ -34,12 +41,51 @@ export class AsyncMessenger<Broker, M, A> {
     this.broker = broker;
     this.#send = send;
     this.#stop = stop;
+
+    const tickMs = 100;
+    const maxDurationMs = 5000;
+    this.#timer = setInterval(() => {
+      const currentDate = Date.now();
+      const item = this.#pendingOperations.peek(); // O(1)
+      if (item !== undefined) {
+        const delta = currentDate - item.date;
+        if (this.#doneIds.has(item.message.id)) {
+          this.#pendingOperations.pop(); // O(log N)
+          this.#doneIds.delete(item.message.id); // O(1)
+
+          console.log(
+            "done safely",
+            item.message.id,
+            "currentSize",
+            this.#pendingOperations.length,
+          );
+        } else if (delta >= maxDurationMs) {
+          this.receive({
+            id: item.message.id,
+            error: `timeout exceeded for id ${item.message.id}`,
+          });
+
+          this.#pendingOperations.pop(); // O(log N)
+          // this.#stop(broker);
+
+          console.log(
+            "Removing id",
+            item.message.id,
+            "op",
+            item.message.op,
+            "currentSize",
+            this.#pendingOperations.length,
+          );
+        }
+      }
+    }, tickMs);
   }
 
   async terminate(): Promise<void> {
     await Promise.all([...this.#tasks.values()].map((t) => t.promise));
     logger.info(`close worker ${this.constructor.name}`);
     this.#stop(this.broker);
+    clearInterval(this.#timer);
   }
 
   execute(
@@ -50,13 +96,26 @@ export class AsyncMessenger<Broker, M, A> {
     const id = this.nextId();
     const promise = deferred<unknown>();
     this.#tasks.set(id, { promise, hooks });
-    void this.#send(this.broker, { id, op, data });
+
+    const message = { id, op, data };
+    this.#pendingOperations.push({
+      date: Date.now(),
+      message,
+    });
+    void this.#send(this.broker, message);
     return promise;
   }
 
   async receive(answer: Answer<A>): Promise<void> {
     const { id } = answer;
     const { promise, hooks } = this.#tasks.get(id)!;
+    this.#doneIds.add(answer.id);
+    console.log(
+      "Removing id",
+      answer.id,
+      "currentSize",
+      this.#pendingOperations.length,
+    );
     if (answer.error) {
       promise.reject(new Error(answer.error));
     } else {
