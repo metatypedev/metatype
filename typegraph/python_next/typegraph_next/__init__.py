e9fbc414f743945975ea670f958769f8e296eed3
Natoandro <anatoandro@hotmail.com>
2023-08-08 12:41:53 +0300

    refactor(sdk/python): Use decorator instead of Context block

--- typegraph/python_next/typegraph_next/__init__.py
+++ typegraph/python_next/typegraph_next/__init__.py
@@ -1,6 +1,7 @@
 # Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
 # SPDX-License-Identifier: MPL-2.0
 
-from typegraph_next.graph.typegraph import typegraph  # noqa
+from typegraph_next.graph.typegraph import typegraph, G  # noqa
+from typegraph_next.policy import Policy  # noqa
 
 version = "0.1.13-0+dev"
--- typegraph/python_next/typegraph_next/graph/typegraph.py
+++ typegraph/python_next/typegraph_next/graph/typegraph.py
@@ -1,9 +1,10 @@
 # Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
 # SPDX-License-Identifier: MPL-2.0
 
+from dataclasses import dataclass
 import inspect
 from pathlib import Path
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Callable, List, Optional
 
 from typegraph_next.gen.exports.core import TypegraphInitParams
 from typegraph_next.gen.types import Err
@@ -13,12 +14,12 @@ if TYPE_CHECKING:
     from typegraph_next import t
 
 
-class typegraph:
+class Typegraph:
     name: str
     dynamic: Optional[bool]
     folder: Optional[str]
     path: str
-    _context: List["typegraph"] = []
+    _context: List["Typegraph"] = []
 
     def __init__(
         self, name: str, dynamic: Optional[bool] = None, folder: Optional[str] = None
@@ -28,26 +29,8 @@ class typegraph:
         self.folder = folder
         self.path = str(Path(inspect.stack()[1].filename).resolve().parent)
 
-    def __enter__(self):
-        self._context.append(self)
-        core.init_typegraph(
-            store,
-            TypegraphInitParams(
-                name=self.name, dynamic=self.dynamic, folder=self.folder, path=self.path
-            ),
-        )
-        return self
-
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        tg = self._context.pop()
-        assert tg == self
-        res = core.finalize_typegraph(store)
-        if isinstance(res, Err):
-            raise Exception(res.value)
-        print(res.value)
-
     @classmethod
-    def get_active(cls) -> Optional["typegraph"]:
+    def get_active(cls) -> Optional["Typegraph"]:
         if len(cls._context) == 0:
             raise Exception("No active typegraph")
         return cls._context[-1]
@@ -60,3 +43,40 @@ class typegraph:
         res = core.expose(store, lst, [])
         if isinstance(res, Err):
             raise Exception(res.value)
+
+
+@dataclass
+class G:
+    typegraph: Typegraph
+
+    def expose(self, **kwargs: "t.func"):
+        self.typegraph.expose(**kwargs)
+
+
+def typegraph(
+    name: str, dynamic: Optional[bool] = None, folder: Optional[str] = None
+) -> Callable[[Callable[[G], None]], Typegraph]:
+    def wrapper(builder: G) -> Typegraph:
+        tg = Typegraph(name, dynamic, folder)
+        Typegraph._context.append(tg)
+        core.init_typegraph(
+            store,
+            TypegraphInitParams(
+                name=name, dynamic=dynamic, folder=folder, path=tg.path
+            ),
+        )
+
+        builder(G(tg))
+
+        popped = Typegraph._context.pop()
+        assert tg == popped
+
+        res = core.finalize_typegraph(store)
+        if isinstance(res, Err):
+            raise Exception(res.value)
+
+        print(res.value)
+
+        return tg
+
+    return wrapper
