59450b7aacef542713aac414bd46438770a8afd9
Natoandro <anatoandro@hotmail.com>
2023-08-09 17:54:17 +0300

    Fix file headers

--- typegraph/python_next/typegraph_next/runtimes/base.py
+++ typegraph/python_next/typegraph_next/runtimes/base.py
@@ -1,3 +1,6 @@
+# Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+# SPDX-License-Identifier: MPL-2.0
+
 from dataclasses import dataclass
 
 from typegraph_next.gen.exports.core import MaterializerId, RuntimeId
