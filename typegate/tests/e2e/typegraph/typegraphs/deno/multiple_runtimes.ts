dfe11dce237aaba0b88c3b81e884a1019d7fd6a6
Natoandro <anatoandro@hotmail.com>
2023-08-21 19:58:53 +0300

    Add test for multiple runtimes

---
+++ typegate/tests/e2e/typegraph/typegraphs/deno/multiple_runtimes.ts
@@ -0,0 +1,22 @@
+import { Policy, t, typegraph } from "@typegraph/deno/src/mod.ts";
+import { DenoRuntime } from "@typegraph/deno/src/runtimes/deno.ts";
+import { PythonRuntime } from "@typegraph/deno/src/runtimes/python.ts";
+
+typegraph("test-multiple-runtimes", (g) => {
+  const pub = Policy.public();
+  const deno = new DenoRuntime();
+  const python = new PythonRuntime();
+
+  g.expose({
+    add: t.func(
+      t.struct({ "first": t.float(), "second": t.float() }),
+      t.float(),
+      python.fromLambda("lambda x: x['first'] + x['second']"),
+    ).withPolicy(pub),
+    multiply: deno.func(
+      t.struct({ "first": t.float(), "second": t.float() }),
+      t.float(),
+      { code: "({first, second}) => first * second" },
+    ).withPolicy(pub),
+  });
+});
--- typegate/tests/utils/test.ts
+++ typegate/tests/utils/test.ts
@@ -9,7 +9,7 @@ import { shell } from "./shell.ts";
 
 import { Server } from "std/http/server.ts";
 import { assertSnapshot } from "std/testing/snapshot.ts";
-import { assertEquals, assertNotEquals } from "std/testing/asserts.ts";
+import { assertEquals, assertNotEquals } from "std/assert/mod.ts";
 import { Engine } from "../../src/engine.ts";
 import { Typegate } from "../../src/typegate/mod.ts";
 import { ConnInfo } from "std/http/server.ts";
@@ -198,6 +198,7 @@ export class MetaTest {
     assertNotEquals(paths.length, 0);
     const first = paths.shift()!;
     const expected = await this.serialize(first, { pretty: true });
+    await this.assertSnapshot(expected);
     for (const path of paths) {
       await this.should(
         `serialize ${path} to the same typegraph as ${first}`,
