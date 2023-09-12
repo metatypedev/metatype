87e8377c562cb40e3b6c02f83a1905988f1d83c1
Michaël <afmichael73@gmail.com>
2023-09-12 14:34:49 +0300

    feat(sdk): prepare core utils

---
+++ typegraph/core/src/utils/apply.rs
@@ -0,0 +1,185 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
+use std::collections::HashMap;
+
+use crate::{
+    errors::Result,
+    wit::utils::{Apply, ApplyPath, ApplyValue},
+};
+
+#[derive(Debug, Clone)]
+pub struct PathTree {
+    pub entries: Vec<PathTree>,
+    pub name: String,
+    pub value: ApplyValue,
+}
+
+impl PathTree {
+    fn new(name: String, value: ApplyValue) -> PathTree {
+        Self {
+            entries: vec![],
+            name,
+            value,
+        }
+    }
+
+    // pub fn is_leaf(&self) -> bool {
+    //     self.entries.len() == 0
+    // }
+
+    fn build_helper(
+        parent: &mut PathTree,
+        description: &ApplyPath,
+        depth: usize,
+    ) -> Result<(), String> {
+        if depth < description.path.len() {
+            let chunk = &description.path[depth];
+            let child = match parent.find(chunk) {
+                Some(child) => child,
+                None => {
+                    parent.add(PathTree::new(chunk.to_string(), description.value.clone()));
+                    parent
+                        .find(chunk)
+                        .ok_or("node incorrectly added into tree".to_string())?
+                }
+            };
+            PathTree::build_helper(child, description, depth + 1)?
+        }
+        Ok(())
+    }
+
+    pub fn build_from(apply: &Apply) -> Result<PathTree, String> {
+        let mut root = PathTree::new(
+            "root".to_string(),
+            ApplyValue {
+                inherit: false,
+                payload: None,
+            },
+        );
+        for descr in apply.paths.iter() {
+            PathTree::build_helper(&mut root, descr, 0)?;
+        }
+        Ok(root)
+    }
+
+    pub fn find(&mut self, other_name: &str) -> Option<&mut PathTree> {
+        self.entries
+            .iter_mut()
+            .find(|entry| entry.name.eq(other_name))
+    }
+
+    pub fn add(&mut self, entry: PathTree) {
+        self.entries.push(entry);
+    }
+
+    fn print_helper(lines: &mut String, node: &PathTree, depth: u32) {
+        if depth == 0 {
+            lines.push_str(&node.name);
+        } else {
+            let name = node.name.clone();
+            let inherit = node.value.inherit;
+            lines.push_str(&format!(
+                "{:indent$}└── [{name} ({inherit})]",
+                "",
+                indent = ((depth as usize) - 1) * 4
+            ));
+        }
+
+        for entry in node.entries.iter() {
+            PathTree::print_helper(lines, entry, depth + 1);
+        }
+    }
+}
+
+impl ToString for PathTree {
+    fn to_string(&self) -> String {
+        let mut lines = String::new();
+        PathTree::print_helper(&mut lines, self, 0);
+        lines
+    }
+}
+
+// Type utility for wrapping a node with index information
+#[derive(Debug, Clone)]
+pub struct ItemNode {
+    pub parent_index: Option<u32>,
+    pub index: u32,
+    pub node: PathTree,
+}
+
+// scheme similar to `types: TypeNode[]` in typegate
+// item node wrappers are ordered in such a way that
+// the top items are the leaves
+pub fn flatten_to_items_array(path_tree: &PathTree) -> Result<Vec<ItemNode>, String> {
+    let mut index = 0;
+    let mut levels = vec![vec![ItemNode {
+        parent_index: None,
+        index,
+        node: path_tree.clone(),
+    }]];
+
+    loop {
+        if let Some(parents) = levels.last() {
+            let mut current: Vec<ItemNode> = vec![];
+            for parent in parents {
+                for entry in parent.node.entries.iter() {
+                    index += 1;
+                    current.push(ItemNode {
+                        parent_index: Some(parent.index),
+                        index,
+                        node: entry.clone(),
+                    });
+                }
+            }
+            if current.is_empty() {
+                // all leaves reached
+                break;
+            }
+            levels.push(current);
+        } else {
+            panic!("first level must be populated");
+        }
+    }
+
+    // flatten the tree to a 1D array
+    let final_size = (index + 1) as usize;
+    let mut tmp_list = vec![None; final_size];
+
+    for level in levels {
+        for entity in level {
+            let pos = entity.index as usize;
+            match tmp_list.get(pos).unwrap() {
+                None => {
+                    tmp_list[pos] = Some(entity);
+                }
+                Some(value) => {
+                    return Err(format!(
+                        "index {} is already filled with {:?}",
+                        entity.index, value
+                    ))
+                }
+            }
+        }
+    }
+
+    let mut result = vec![];
+    for (i, item) in tmp_list.iter().enumerate() {
+        result.push(item.clone().ok_or(format!("index {} is vacant", i))?)
+    }
+
+    Ok(result)
+}
+
+pub fn build_parent_to_child_indices(item_list: &Vec<ItemNode>) -> HashMap<u32, Vec<u32>> {
+    let mut map: HashMap<u32, Vec<u32>> = HashMap::new();
+
+    for item in item_list {
+        if let Some(parent_index) = item.parent_index {
+            map.entry(parent_index).or_insert_with(Vec::new);
+            map.get_mut(&parent_index).unwrap().push(item.index);
+        }
+    }
+
+    map
+}
--- typegraph/core/src/utils/mod.rs
+++ typegraph/core/src/utils/mod.rs
@@ -6,118 +6,24 @@ use std::collections::HashMap;
 use crate::{
     errors::Result,
     global_store::with_store,
-    types::{Type, TypeFun},
-    wit::{
-        core::{Core, TypeBase, TypeId, TypeStruct, TypeWithInjection},
-        utils::{Apply, ApplyPath, ApplyValue},
-    },
+    wit::core::{Core, TypeBase, TypeId, TypeStruct, TypeWithInjection},
     Lib,
 };
 
-#[derive(Debug, Clone)]
-struct PathTree {
-    // pub parent: Option<&'a mut Box<PathTree<'a>>>,
-    pub entries: Vec<PathTree>,
-    pub name: String,
-    pub value: ApplyValue,
-}
-
-impl PathTree {
-    fn new(name: String, value: ApplyValue) -> PathTree {
-        Self {
-            entries: vec![],
-            name,
-            value,
-        }
-    }
-
-    // pub fn is_leaf(&self) -> bool {
-    //     self.entries.len() == 0
-    // }
-
-    fn build_helper(
-        parent: &mut PathTree,
-        description: &ApplyPath,
-        depth: usize,
-    ) -> Result<(), String> {
-        if depth < description.path.len() {
-            let item = &description.path[depth];
-            let child = match parent.find(item) {
-                Some(child) => child,
-                None => {
-                    parent.add(PathTree::new(item.to_string(), description.value.clone()));
-                    parent
-                        .find(item)
-                        .ok_or("node incorrectly added into tree".to_string())?
-                }
-            };
-            PathTree::build_helper(child, description, depth + 1)?
-        }
-        Ok(())
-    }
-
-    pub fn build_from(apply: &Apply) -> Result<PathTree, String> {
-        let mut root = PathTree::new(
-            "root".to_string(),
-            ApplyValue {
-                inherit: false,
-                payload: None,
-            },
-        );
-        for descr in apply.paths.iter() {
-            PathTree::build_helper(&mut root, descr, 0)?;
-        }
-        Ok(root)
-    }
-
-    pub fn find(&mut self, other_name: &str) -> Option<&mut PathTree> {
-        self.entries
-            .iter_mut()
-            .find(|entry| entry.name.eq(other_name))
-    }
-
-    pub fn add(&mut self, entry: PathTree) {
-        self.entries.push(entry);
-    }
-
-    fn print_helper(lines: &mut String, node: &PathTree, depth: u32) {
-        if depth == 0 {
-            lines.push_str(&node.name);
-        } else {
-            let name = node.name.clone();
-            let inherit = node.value.inherit;
-            lines.push_str(&format!(
-                "{:indent$}└── [{name} ({inherit})]",
-                "",
-                indent = ((depth as usize) - 1) * 4
-            ));
-        }
-
-        for entry in node.entries.iter() {
-            PathTree::print_helper(lines, entry, depth + 1)
-        }
-    }
-}
-
-impl ToString for PathTree {
-    fn to_string(&self) -> String {
-        let mut lines = String::new();
-        PathTree::print_helper(&mut lines, self, 0);
-        lines
-    }
-}
+mod apply;
 
 impl crate::wit::utils::Utils for crate::Lib {
     fn gen_applyb(supertype_id: TypeId, apply: crate::wit::utils::Apply) -> Result<TypeId> {
         // Walk supertype and enumerate ALL possible paths
         // get that path
 
-        let _apply_tree = PathTree::build_from(&apply)?;
+        let apply_tree = apply::PathTree::build_from(&apply)?;
+        let item_list = apply::flatten_to_items_array(&apply_tree)?;
+        let p2c_indices = apply::build_parent_to_child_indices(&item_list);
 
-        // print_tree(&apply_tree, 0);
-
-        // let root_props: Vec<(String, u32)> = vec![];
-        let mut obj_cache: HashMap<String, TypeId> = HashMap::new();
+        if true {
+            return Err(format!("!!! {:?}", p2c_indices));
+        }
 
         // finalize all possible paths
         let mut finalized_paths: HashMap<Vec<String>, TypeId> = HashMap::new();
@@ -141,38 +47,6 @@ impl crate::wit::utils::Utils for crate::Lib {
                 })?;
                 finalized_paths.insert(field.path.clone(), new_id);
             }
-        }
-
-        for (path, _leaf_tpe_id) in finalized_paths.iter() {
-            let _field_name = path.last().ok_or("invalid empty path encountered")?;
-            let _id = with_store(|s| -> Result<u32, String> {
-                let (_tpe, id) = s.get_type_by_path(supertype_id, path)?;
-                Ok(id)
-            })?;
-
-            let mut curr_path = vec![];
-            for chunk in path {
-                curr_path.push(chunk.clone());
-                let is_leaf = path.len() == curr_path.len();
-                if !is_leaf {
-                    let data = with_store(|s| -> Result<TypeStruct> {
-                        let (tpe, _id) = s.get_type_by_path(supertype_id, &curr_path)?;
-                        if let Type::Struct(t) = tpe {
-                            return Ok(t.data.clone());
-                        }
-                        Err(format!(
-                            "struct was expected, got {:?} at {}",
-                            tpe.get_concrete_type_name(),
-                            curr_path.join(".")
-                        ))
-                    })?;
-                    let id = Lib::structb(data, TypeBase::default())?;
-                    obj_cache.insert(curr_path.join("."), id);
-                }
-                // let new_id = Lib::structb(t.data.clone(), TypeBase::default());
-            }
-
-            // root_props.push((field_name.clone(), inner_tpe));
         }
 
         // Note: if used out of scope even with proper imports Lib::some_type() panics
