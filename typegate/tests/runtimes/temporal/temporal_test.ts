fa0bcd1e0759c522df561f866ec4ed7ac6bd9f52
Michaël <afmichael73@gmail.com>
2023-09-21 17:13:15 +0300

    test(python): temporal runtime serialization

---
+++ typegate/tests/runtimes/temporal/temporal_test.ts
@@ -0,0 +1,17 @@
+// Copyright Metatype OÜ, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
+
+import { Meta } from "../../utils/mod.ts";
+import { MetaTest } from "../../utils/test.ts";
+
+async function testSerialize(t: MetaTest, file: string) {
+  await t.should("serialize python typegraph", async () => {
+    const tg = await Meta.cli("serialize", "--pretty", "-f", file);
+    await t.assertSnapshot(tg);
+  });
+}
+
+Meta.test("temporal", async (t) => {
+  await testSerialize(t, "runtimes/temporal/temporal.py");
+  // await testSerialize(t, "runtimes/temporal/temporal.ts");
+});
