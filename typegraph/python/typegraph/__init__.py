42a95004e1c3a42d1c1d49d193a9c6a09d967bb7
Natoandro <anatoandro@hotmail.com>
2023-10-03 11:27:48 +0300

    chore: Remove old typegraph SDK

--- typegraph/python/typegraph/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
-# SPDX-License-Identifier: MPL-2.0
-
-# shortcut imports
-from typegraph import types as t  # noqa
-from typegraph.graph.typegraph import TypeGraph  # noqa
-
-version = "0.2.0-alpha.1"
