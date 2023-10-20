8d0af34eb2d7028bc5884f58002474604ce4fc98
Natoandro <anatoandro@hotmail.com>
2023-10-20 11:06:04 +0300

    wip

--- website/use-cases/microservice-orchestration/t.py
+++ website/use-cases/microservice-orchestration/t.py
@@ -1,38 +1,40 @@
 # skip:start
 from os import environ
 
-from typegraph import TypeGraph, policies, t
-from typegraph.runtimes.deno import DenoRuntime
-from typegraph.runtimes.graphql import GraphQLRuntime
+from typegraph import typegraph, Policy, t, Graph
+from typegraph.graph.params import Cors
+from typegraph.runtimes import DenoRuntime, GraphQLRuntime
+
 
 # skip:end
-with TypeGraph(
-    "team-a",
+@typegraph(
     # skip:next-line
-    cors=TypeGraph.Cors(allow_origin=["https://metatype.dev", "http://localhost:3000"]),
-) as g1:
-    public = policies.public()
+    cors=Cors(allow_origin=["https://metatype.dev", "http://localhost:3000"]),
+)
+def team_a(g: Graph):
+    public = Policy.public()
 
     deno = DenoRuntime()
     records = GraphQLRuntime(environ.get("TG_URL", "http://localhost:7890") + "/team-b")
 
-    g1.expose(
-        version_team_b=records.query(t.struct({}), t.integer(), path=("version",)),
+    g.expose(
+        public,
+        version_team_b=records.query(t.struct({}), t.integer(), path=["version"]),
         version_team_a=deno.static(t.integer(), 3),
-        default_policy=[public],
     )
 
-with TypeGraph(
-    "team-b",
-    # skip:next-line
-    cors=TypeGraph.Cors(allow_origin=["https://metatype.dev", "http://localhost:3000"]),
-) as g2:
-    public = policies.public()
 
-    deno = DenoRuntime()
-
-    g2.expose(
-        version=deno.static(t.integer(), 12),
-        record=deno.static(t.struct({"weight": t.integer()}), {"weight": 100}),
-        default_policy=[public],
-    )
+# @typegraph(
+#     # skip:next-line
+#     cors=Cors(allow_origin=["https://metatype.dev", "http://localhost:3000"]),
+# )
+# def team_b(g: Graph):
+#     public = Policy.public()
+#
+#     deno = DenoRuntime()
+#
+#     g.expose(
+#         public,
+#         version=deno.static(t.integer(), 12),
+#         record=deno.static(t.struct({"weight": t.integer()}), {"weight": 100}),
+#     )
