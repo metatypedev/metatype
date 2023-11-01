3611a6fdd8af1ef0f4fae1929c03c205b732f32e
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2023-11-01 15:45:36 +0000

    refactor: stub out  `native` crate

--- typegate/native/src/runtimes/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright Metatype OÜ, licensed under the Elastic License 2.0.
-// SPDX-License-Identifier: Elastic-2.0
-
-mod prisma;
-mod python;
-mod temporal;
-mod wasmedge;
--- typegate/native/src/runtimes/python/mod.rs
+++
@@ -1,6 +0,0 @@
-// Copyright Metatype OÜ, licensed under the Elastic License 2.0.
-// SPDX-License-Identifier: Elastic-2.0
-
-pub mod python_bindings;
-pub mod wasi_vm;
-pub mod wasmedge_sdk_bindgen;
