ec3bec1d04897bf708237109cc58ea96f2a3a9b2
afmika <afmichael73@gmail.com>
2023-11-17 17:19:02 +0300

    feat(prisma): add cursor

--- typegraph/core/src/runtimes/prisma/type_generation/additional_filters.rs
+++ typegraph/core/src/runtimes/prisma/type_generation/additional_filters.rs
@@ -57,3 +57,40 @@ impl TypeGen for Distinct {
         format!("_KeysOf_{model_name}")
     }
 }
+
+pub struct Cursor {
+    model_id: TypeId,
+}
+
+impl Cursor {
+    pub fn new(model_id: TypeId) -> Self {
+        Self { model_id }
+    }
+}
+
+impl TypeGen for Cursor {
+    fn generate(&self, context: &PrismaContext) -> Result<TypeId> {
+        let model = context.model(self.model_id)?;
+        let model = model.borrow();
+
+        let fields = model
+            .iter_props()
+            .filter_map(|(k, prop)| match prop {
+                Property::Scalar(t) => Some((k.to_string(), t.type_id)),
+                _ => None,
+            })
+            .collect::<Vec<(String, TypeId)>>();
+
+        let mut variants = vec![];
+        for (k, id) in fields {
+            let variant = t::struct_().prop(k, id).build()?;
+            variants.push(variant)
+        }
+        t::union(variants).named(self.name()).build()
+    }
+
+    fn name(&self) -> String {
+        let model_name = self.model_id.type_name().unwrap().unwrap();
+        format!("_{}_Cursor", model_name)
+    }
+}
