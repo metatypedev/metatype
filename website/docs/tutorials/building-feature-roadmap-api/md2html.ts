9ccb7c8defad61602501749ee992c5401fc24048
Natoandro <anatoandro@hotmail.com>
2023-11-20 20:56:12 +0300

    fix merge confusions

--- website/docs/tutorials/building-feature-roadmap-api/md2html.ts
+++
@@ -1,8 +0,0 @@
-// Copyright Metatype OÜ, licensed under the Elastic License 2.0.
-// SPDX-License-Identifier: Elastic-2.0
-
-import * as marked from "https://deno.land/x/marked/mod.ts";
-
-export function parse({ raw }: { raw: string }): string {
-  return marked.parse(raw);
-}
