c15dfdae8ff15db3685f4baef52843dc3fe52a1e
Natoandro <anatoandro@hotmail.com>
2023-12-04 20:54:20 +0300

    Fix e2e tests

--- typegate/tests/e2e/typegraph/validator_test.ts
+++ typegate/tests/e2e/typegraph/validator_test.ts
@@ -12,7 +12,7 @@ Meta.test("typegraph validation", async (t) => {
     "fail to serialize typegraph with invalid injection",
     async () => {
       try {
-        await m.cli({}, "serialize", "-f", "typegraphs/python/validator.py");
+        await m.cli({}, "serialize", "-f", "validator.py");
         assert(false, "should have thrown");
       } catch (e) {
         await t.assertSnapshot(e.message);
