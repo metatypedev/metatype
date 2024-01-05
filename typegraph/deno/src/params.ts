3fd31ad123666bc771af389beefae4d5071e640d
Natoandro <anatoandro@hotmail.com>
2024-01-05 22:14:58 +0300

    feat: Deno front-end

--- typegraph/deno/src/params.ts
+++ typegraph/deno/src/params.ts
@@ -3,6 +3,29 @@
 
 import { RawAuth } from "./typegraph.ts";
 import { Auth as Auth_, wit_utils } from "./wit.ts";
+import * as t from "./types.ts";
+
+export type StdOauth2Profiler =
+  | { profiler: "default" }
+  | { profiler: "none" }
+  | { profiler: "extended", extension: any }
+  | { profiler: "custom", id: number };
+
+export function noProfiler(): StdOauth2Profiler {
+  return { profiler: "none" };
+}
+
+export function defaultProfiler(): StdOauth2Profiler {
+  return { profiler: "default" };
+}
+
+export function extendedProfiler(extension: any): StdOauth2Profiler {
+  return { profiler: "extended", extension };
+}
+
+export function customProfiler(func: t.Typedef): StdOauth2Profiler {
+  return { profiler: "custom", id: func._id };
+}
 
 export class Auth {
   static jwt(name: string, format: string, algorithm?: any): Auth_ {
@@ -39,59 +62,72 @@ export class Auth {
     };
   }
 
-  static oauth2Digitalocean(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("digitalocean", scopes));
+  private static stdOauth2(provider: string, scopes: string, profiler: StdOauth2Profiler): RawAuth {
+    switch (profiler.profiler) {
+      case "none":
+        return new RawAuth(wit_utils.oauth2WithoutProfiler(provider, scopes));
+      case "extended":
+        return new RawAuth(wit_utils.oauth2WithExtendedProfiler(provider, scopes, JSON.stringify(profiler.extension)));
+      case "custom":
+        return new RawAuth(wit_utils.oauth2WithCustomProfiler(provider, scopes, profiler.id));
+      default:
+        return new RawAuth(wit_utils.oauth2(provider, scopes));
+    }
   }
 
-  static oauth2Discord(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("discord", scopes));
+  static oauth2Digitalocean(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("digitalocean", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Dropbox(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("dropbox", scopes));
+  static oauth2Discord(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("discord", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Facebook(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("facebook", scopes));
+  static oauth2Dropbox(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("dropbox", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Github(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("github", scopes));
+  static oauth2Facebook(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("facebook", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Gitlab(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("gitlab", scopes));
+  static oauth2Github(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("github", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Google(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("google", scopes));
+  static oauth2Gitlab(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("gitlab", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Instagram(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("instagram", scopes));
+  static oauth2Google(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("google", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Linkedin(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("linkedin", scopes));
+  static oauth2Instagram(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("instagram", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Microsoft(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("microsoft", scopes));
+  static oauth2Linkedin(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("linkedin", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Reddit(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("reddit", scopes));
+  static oauth2Microsoft(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("microsoft", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Slack(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("slack", scopes));
+  static oauth2Reddit(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("reddit", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Stackexchange(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("stackexchange", scopes));
+  static oauth2Slack(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("slack", scopes, profiler ?? defaultProfiler());
   }
 
-  static oauth2Twitter(scopes: string): RawAuth {
-    return new RawAuth(wit_utils.oauth2("twitter", scopes));
+  static oauth2Stackexchange(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("stackexchange", scopes, profiler ?? defaultProfiler());
+  }
+
+  static oauth2Twitter(scopes: string, profiler?: StdOauth2Profiler): RawAuth {
+    return Auth.stdOauth2("twitter", scopes, profiler ?? defaultProfiler());
   }
 }
