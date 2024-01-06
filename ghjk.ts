1e83772c7ea28228dec7b39e13d64accd65baa98
Natoandro <anatoandro@hotmail.com>
2024-01-06 08:17:58 +0300

    update ghjk

--- ghjk.ts
+++ ghjk.ts
@@ -1,6 +1,6 @@
-export { ghjk } from "https://raw.github.com/metatypedev/ghjk/dc9b402/mod.ts";
-import * as ghjk from "https://raw.github.com/metatypedev/ghjk/dc9b402/mod.ts";
-import * as ports from "https://raw.github.com/metatypedev/ghjk/dc9b402/ports/mod.ts";
+export { ghjk } from "https://raw.github.com/metatypedev/ghjk/1dc9d1f/mod.ts";
+import * as ghjk from "https://raw.github.com/metatypedev/ghjk/1dc9d1f/mod.ts";
+import * as ports from "https://raw.github.com/metatypedev/ghjk/1dc9d1f/ports/mod.ts";
 
 const PROTOC_VERSION = "v24.1";
 const POETRY_VERSION = "1.7.0";
