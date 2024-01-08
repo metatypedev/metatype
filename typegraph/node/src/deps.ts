14d1f7d09b2727f6f381d2447b2559ad1324f528
afmika <afmichael73@gmail.com>
2024-01-08 16:37:53 +0300

    feat(sdk): replace deno dep to node equivalent

--- typegraph/node/src/deps.ts
+++ typegraph/node/src/deps.ts
@@ -1,10 +1,6 @@
 // Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
 // SPDX-License-Identifier: MPL-2.0
-
-export { mapValues } from "https://deno.land/std@0.192.0/collections/map_values.ts";
-export { expandGlobSync } from "https://deno.land/std@0.192.0/fs/mod.ts";
-export { default as caller } from "https://deno.land/x/caller@0.1.4/caller.ts";
-export {
-  dirname,
-  fromFileUrl,
-} from "https://deno.land/std@0.192.0/path/mod.ts";
+export { default as mapValues } from "lodash/mapValues";
+export { default as caller } from "caller";
+export { fileURLToPath as fromFileUrl } from "node:url";
+export { dirname } from "node:path";
