63ec3afb0735bc92ac08d9225aa9697ff5702806
afmika <afmichael73@gmail.com>
2024-01-18 18:04:20 +0300

    fix(website): uncomment rate declaration

--- website/typegraphs/rate.ts
+++ website/typegraphs/rate.ts
@@ -11,11 +11,11 @@ typegraph({
   const pub = Policy.public();
 
   g.expose({
-    lightweight_call: random.gen(t.string()), //.rate(calls:true, weight:1),
-    medium_call: random.gen(t.string()), //.rate(calls:true, weight:5),
-    heavy_call: random.gen(t.string()), //.rate(calls:true, weight:15),
+    lightweight_call: random.gen(t.string()).rate({ calls: true, weight: 1 }),
+    medium_call: random.gen(t.string()).rate({ calls: true, weight: 5 }),
+    heavy_call: random.gen(t.string()).rate({ calls: true, weight: 15 }),
     by_result_count: random.gen(
       t.list(t.string()),
-    ), //.rate(calls:false, weight:2),  // increment by # of results returned
+    ).rate({ calls: false, weight: 2 }), // increment by # of results returned
   }, pub);
 });
