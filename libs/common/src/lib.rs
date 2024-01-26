e3c69538874cb878cb2d24857b6de3bc738577e4
afmika <afmichael73@gmail.com>
2024-01-26 16:37:03 +0300

    fix(sdk): disable incompatible feat

--- libs/common/src/lib.rs
+++ libs/common/src/lib.rs
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: Elastic-2.0
 
 pub mod archive;
-pub mod graphql;
-pub mod node;
 pub mod typegraph;
+
+// Note:
+// try refactoring the typegraph deploy feature and refactor redundant code on both sdks
+// once WebAssembly async and networking has matured enough
+#[cfg(not(target_arch = "wasm32"))]
+pub mod graphql;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod node;
