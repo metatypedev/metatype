e0433f432695bb26a9ad38fe0185cf3661f8fe39
afmika <afmichael73@gmail.com>
2024-02-08 15:10:02 +0300

    fix(test): same port issue

--- typegate/tests/e2e/self_deploy/self_deploy_test.ts
+++ typegate/tests/e2e/self_deploy/self_deploy_test.ts
@@ -1,23 +1,19 @@
 // Copyright Metatype OÜ, licensed under the Elastic License 2.0.
 // SPDX-License-Identifier: Elastic-2.0
-
-import { Meta, sleep } from "test-utils/mod.ts";
-import { deploy, undeploy } from "./self_deploy.ts";
 import { BasicAuth } from "@typegraph/sdk/tg_deploy.js";
+
+import { Meta } from "test-utils/mod.ts";
+import { deploy, undeploy } from "./self_deploy.ts";
 import { testDir } from "test-utils/dir.ts";
 import { join } from "std/path/join.ts";
 
-const port = 7893;
+const port = 7898;
 const auth = new BasicAuth("admin", "password");
 const gate = `http://localhost:${port}`;
 const cliVersion = "0.3.3";
 const cwdDir = join(testDir, "e2e", "self_deploy");
 
 Meta.test("deploy and undeploy typegraph without meta-cli", async (_) => {
-  const waitMs = 10000;
-  console.log("Waiting", waitMs / 1000, "seconds");
-  await sleep(waitMs);
-
   await deploy(gate, auth, cliVersion, cwdDir);
   await undeploy(gate, auth);
 }, { port, systemTypegraphs: true });
