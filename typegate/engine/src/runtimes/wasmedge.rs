907913dd9a804a2fd79c6cae66863dda28942989
afmika <afmichael73@gmail.com>
2024-03-28 20:22:20 +0300

    feat(gate,native): interface (wip)

--- typegate/engine/src/runtimes/wasmedge.rs
+++ typegate/engine/src/runtimes/wasmedge.rs
@@ -10,11 +10,16 @@ use anyhow::bail;
 #[rustfmt::skip]
 use deno_core as deno_core; // necessary for re-exported macros to work
 
-use wasmedge_sdk::VmBuilder;
+use once_cell::sync::OnceCell;
+use wasmedge_sdk::config::CommonConfigOptions;
+use wasmedge_sdk::error::HostFuncError;
 use wasmedge_sdk::{
     config::{ConfigBuilder, HostRegistrationConfigOptions},
     dock::{Param, VmDock},
-    Module,
+    params, Module,
+};
+use wasmedge_sdk::{
+    host_function, Caller, ImportObject, ImportObjectBuilder, NeverType, VmBuilder, WasmValue,
 };
 
 use std::{env, fs};
@@ -50,20 +55,21 @@ fn param_cast(out: &str, res: &mut Vec<Box<dyn Any + Send + Sync>>) -> Result<St
     Ok(json)
 }
 
+fn load_wasm_module_any_ext<P: AsRef<Path>>(wasm_relative_path: P) -> Result<Module> {
+    let wasm_absolute_path = match env::current_dir() {
+        Ok(cwd) => cwd.join(wasm_relative_path),
+        Err(e) => return Err(anyhow::anyhow!(e)),
+    };
+    let bytes = fs::read(wasm_absolute_path)?;
+    Module::from_bytes(None, bytes).map_err(|e| e.into())
+}
+
 #[deno_core::op2]
 #[string]
 pub fn op_wasmedge_wasi(#[serde] input: WasiInput) -> Result<String> {
     // https://github.com/second-state/wasmedge-rustsdk-examples
 
-    let wasm_relative_path = PathBuf::from(input.wasm);
-
-    let wasm_absolute_path = match env::current_dir() {
-        Ok(cwd) => cwd.join(wasm_relative_path),
-        Err(e) => return Err(anyhow::anyhow!(e)),
-    };
-
-    let bytes = fs::read(wasm_absolute_path).unwrap();
-    let module = Module::from_bytes(None, bytes).unwrap();
+    let module = load_wasm_module_any_ext(input.wasm)?;
 
     let config = ConfigBuilder::default()
         .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
@@ -71,10 +77,8 @@ pub fn op_wasmedge_wasi(#[serde] input: WasiInput) -> Result<String> {
         .unwrap();
     let vm = VmBuilder::new()
         .with_config(config)
-        .build()
-        .unwrap()
-        .register_module(None, module)
-        .unwrap();
+        .build()?
+        .register_module(None, module)?;
 
     let args: Vec<serde_json::Value> = input
         .args
@@ -85,9 +89,60 @@ pub fn op_wasmedge_wasi(#[serde] input: WasiInput) -> Result<String> {
     let params: Vec<Param> = args.iter().map(|v| value_to_param(v).unwrap()).collect();
     let vm = VmDock::new(vm);
 
-    let run = vm.run_func(&input.func, params).unwrap();
+    let run = vm.run_func(&input.func, params)?;
 
     match run {
+        Ok(mut res) => Ok(param_cast(&input.out, &mut res).unwrap()),
+        Err(e) => Err(anyhow::anyhow!(e)),
+    }
+}
+
+static IMPORTS: OnceCell<ImportObject<NeverType>> = OnceCell::new();
+
+#[host_function]
+pub fn gql_host_impl(
+    _caller: Caller,
+    _args: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+    todo!("graphql call");
+}
+
+pub fn get_or_init_imports() -> anyhow::Result<&'static ImportObject<NeverType>> {
+    let import = ImportObjectBuilder::new()
+        .with_func::<(i32, i32), (), NeverType>("gql_host_impl", gql_host_impl, None)?
+        .build::<NeverType>("host", None)?;
+    Ok(IMPORTS.get_or_init(|| import))
+}
+
+#[deno_core::op2]
+#[string]
+pub fn op_wasmedge_mdk_setup(#[serde] input: WasiInput) -> Result<String> {
+    // setup vm
+    let common_options = CommonConfigOptions::default().threads(true);
+    let host_options = HostRegistrationConfigOptions::default().wasi(true);
+    let config = ConfigBuilder::new(common_options)
+        .with_host_registration_config(host_options)
+        .build()?;
+
+    let mut vm = VmBuilder::new().with_config(config).build()?;
+    vm.register_import_module(get_or_init_imports()?)?; // link host function
+
+    let mdk_module = load_wasm_module_any_ext(input.wasm)?;
+    let vm = vm.register_module(None, mdk_module)?;
+
+    // do fn calls
+    vm.run_func(None, "init", params!())?;
+
+    let args: Vec<serde_json::Value> = input
+        .args
+        .iter()
+        .map(|v| serde_json::from_str::<serde_json::Value>(v).unwrap())
+        .collect();
+
+    let params: Vec<Param> = args.iter().map(|v| value_to_param(v).unwrap()).collect();
+    let vm_dock = VmDock::new(vm);
+
+    match vm_dock.run_func(&input.func, params)? {
         Ok(mut res) => Ok(param_cast(&input.out, &mut res).unwrap()),
         Err(e) => Err(anyhow::anyhow!(e)),
     }
--- typegate/tests/runtimes/wasmedge/wasmedge.ts
+++ typegate/tests/runtimes/wasmedge/wasmedge.ts
@@ -4,23 +4,32 @@
 import { Policy, t, typegraph } from "@typegraph/sdk/index.js";
 import { WasmRuntime } from "@typegraph/sdk/runtimes/wasm.js";
 
-export const tg = () =>
-  typegraph("wasmedge_ts", (g: any) => {
-    const pub = Policy.public();
-    const wasm = new WasmRuntime();
+export const tg = await typegraph("wasmedge_ts", (g: any) => {
+  const pub = Policy.public();
+  const wasm = new WasmRuntime();
 
-    g.expose({
-      test_wasi_ts: wasm.fromWasm(
-        t.struct({
-          "a": t.float(),
-          "b": t.float(),
-        }),
-        t.integer(),
-        {
-          func: "add",
-          wasm: "rust.wasm",
-        },
-      ).withPolicy(pub),
-      // test_mdk_ts: mdk.withPolicy(pub),
-    });
+  g.expose({
+    test_wasi_ts: wasm.fromWasm(
+      t.struct({
+        "a": t.float(),
+        "b": t.float(),
+      }),
+      t.integer(),
+      {
+        func: "add",
+        wasm: "rust.wasm",
+      },
+    ).withPolicy(pub),
+    test_mdk_ts: wasm.fromMdk(
+      t.struct({
+        "a": t.float(),
+        "b": t.float(),
+      }),
+      t.integer(),
+      {
+        opName: "add",
+        wasm: "mdk/target/wasm32-wasi/debug/mat_rust.wasm",
+      },
+    ).withPolicy(pub),
   });
+});
