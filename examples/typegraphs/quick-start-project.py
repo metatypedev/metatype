04a0e91ee22bb28fea1dbb816b812ec2ebf6baff
destifo <estifanosbireda@gmail.com>
2024-04-18 15:33:44 +0300

    chore(docs): add first project(python)

---
+++ examples/typegraphs/quick-start-project.py
@@ -0,0 +1,39 @@
+from typegraph.providers.prisma import PrismaRuntime
+from typegraph.runtimes import PythonRuntime
+
+from typegraph import Graph, Policy, t, typegraph
+
+
+@typegraph()
+def example(g: Graph):
+    # access control
+    public = Policy.public()
+
+    # runtimes
+    python = PythonRuntime()
+    db = PrismaRuntime("database", "POSTGRES_CONN")
+
+    # types, database tables
+    message = t.struct(
+        {
+            "id": t.integer(as_id=True, config=["auto"]),
+            "title": t.string(),
+            "body": t.string(),
+        },
+        name="message",
+    )
+
+    # custom functions
+    hello = python.from_lambda(
+        t.struct({"world": t.string()}),
+        t.string(),
+        lambda x: f"Hello {x['world']}!",
+    )
+
+    # expose endpoints
+    g.expose(
+        public,
+        hello=hello,
+        create_message=db.create(message),
+        list_messages=db.find_many(message),
+    )
