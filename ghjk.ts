d2e691fca72737cfdd4275d96d08010e80aff22a
Teo Stocco <zifeo@users.noreply.github.com>
2024-04-24 17:14:55 +0200

    fix: website and headline (#691)
    <!--
    Pull requests are squash merged using:
    - their title as the commit message
    - their description as the commit body
    
    Having a good title and description is important for the users to get
    readable changelog and understand when they need to update his code and
    how.
    -->
    
    <!-- Explain WHAT the change is -->
    
    #### Motivation and context
    
    Fix the CSS issue introduced by docusaurus 3.2.0
    (https://github.com/facebook/docusaurus/issues/10005). 3.2.1 should fix
    it but the affected version maybe loaded by dependencies, so we will
    have to wait a bit more.
    
    <!-- Explain WHY the was made or link an issue number -->
    
    
    <!-- Explain HOW users should update their code when required -->
    
    ### Checklist
    
    - [ ] The change come with new or modified tests
    - [x] Hard-to-understand functions have explanatory comments
    - [ ] End-user documentation is updated to reflect the change
    
    ---------
    
    Co-authored-by: Natoandro <anatoandro@hotmail.com>

--- ghjk.ts
+++ ghjk.ts
@@ -6,7 +6,7 @@ import * as ports from "https://raw.github.com/metatypedev/ghjk/423d38e/ports/mo
 const PROTOC_VERSION = "v24.1";
 const POETRY_VERSION = "1.7.0";
 const PYTHON_VERSION = "3.8.18";
-const PNPM_VERSION = "v8.15.2";
+const PNPM_VERSION = "v9.0.5";
 const WASM_TOOLS_VERSION = "1.0.53";
 const JCO_VERSION = "1.0.0";
 const WASMEDGE_VERSION = "0.13.5";
