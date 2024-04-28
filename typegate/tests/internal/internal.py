3bbd1d8b8c0be1dccdf59438cb34c6515b2e5997
destifo <estifanosbireda@gmail.com>
2024-04-28 13:26:39 +0300

    fix(test): update tg snapshots and tests using denoruntime with module import

--- typegate/tests/internal/internal.py
+++ typegate/tests/internal/internal.py
@@ -1,5 +1,18 @@
-from typegraph import Graph, Policy, t, typegraph
+import os
+import sys
+
+from typegraph.gen.exports.core import (
+    ArtifactResolutionConfig,
+    MigrationAction,
+    MigrationConfig,
+)
+from typegraph.graph.shared_types import BasicAuth
+from typegraph.graph.tg_deploy import TypegraphDeployParams, tg_deploy
+from typegraph.graph.typegraph import Graph
+from typegraph.policy import Policy
 from typegraph.runtimes.deno import DenoRuntime
+
+from typegraph import t, typegraph
 
 
 @typegraph()
@@ -20,3 +33,32 @@ def test_internal(g: Graph):
             inp, out, module="ts/logic.ts", name="remoteSum"
         ).with_policy(public),
     )
+
+
+cwd = sys.argv[1]
+PORT = sys.argv[2]
+gate = f"http://localhost:{PORT}"
+auth = BasicAuth("admin", "password")
+
+tg = test_internal()
+deploy_result = tg_deploy(
+    tg,
+    TypegraphDeployParams(
+        base_url=gate,
+        auth=auth,
+        typegraph_path=os.path.join(cwd, "internal.py"),
+        artifacts_config=ArtifactResolutionConfig(
+            dir=cwd,
+            prefix=None,
+            disable_artifact_resolution=None,
+            codegen=None,
+            prisma_migration=MigrationConfig(
+                migration_dir="prisma-migrations",
+                global_action=MigrationAction(reset=False, create=True),
+                runtime_actions=None,
+            ),
+        ),
+    ),
+)
+
+print(deploy_result.serialized)
