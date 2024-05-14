e9399347b40da42c26a0a1b2a030c75db6c19fd9
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-05-14 03:00:43 +0300

    fix(tests): build test wasm artifacts as needed (#719)
    - Tests now build wasm artifacts on demand.
    
    MET-532
    
    - [x] The change come with new or modified tests
    <!-- 5. Readiness checklist
    - [ ] Hard-to-understand functions have explanatory comments
    - [ ] End-user documentation is updated to reflect the change
    -->

--- typegate/tests/runtimes/wasm_wire/rust/build.sh
+++ typegate/tests/runtimes/wasm_wire/rust/build.sh
@@ -5,9 +5,9 @@ set -e
 pushd ..
 cargo run -p meta-cli -- gen mdk wasm_wire
 popd
-cargo build --target wasm32-unknown-unknown --release
-wasm-opt -Oz ./target/wasm32-unknown-unknown/release/rust.wasm -o ./target/rust-component.wasm.opt
-wasm-tools component new ./target/rust-component.wasm.opt -o ./target/rust-component.wasm
+cargo build --target wasm32-unknown-unknown # --release
+# wasm-opt -Oz ./target/wasm32-unknown-unknown/release/rust.wasm -o ./target/rust-component.wasm.opt
+wasm-tools component new ./target/wasm32-unknown-unknown/debug/rust.wasm -o ./target/rust-component.wasm
 # debug
 wasm-tools component wit target/rust-component.wasm
 
