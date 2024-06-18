62ed64db5abb4de46f4da6114109c94183b37c88
afmika <afmichael73@gmail.com>
2024-06-18 18:14:11 +0300

    refactor!: prep

--- typegraph/deno/sdk/src/gen/interfaces/metatype-typegraph-host.d.ts
+++
@@ -1,7 +0,0 @@
-export namespace MetatypeTypegraphHost {
-  export function print(s: string): void;
-  export function expandPath(root: string, exclude: string[]): string[];
-  export function pathExists(path: string): boolean;
-  export function readFile(path: string): Uint8Array;
-  export function writeFile(path: string, data: Uint8Array): void;
-}
--- typegraph/deno/sdk/src/gen/typegraph_core.d.ts
+++
@@ -1,9 +0,0 @@
-import { MetatypeTypegraphHost } from './interfaces/metatype-typegraph-host.js';
-import { MetatypeTypegraphCore } from './interfaces/metatype-typegraph-core.js';
-import { MetatypeTypegraphRuntimes } from './interfaces/metatype-typegraph-runtimes.js';
-import { MetatypeTypegraphUtils } from './interfaces/metatype-typegraph-utils.js';
-import { MetatypeTypegraphAws } from './interfaces/metatype-typegraph-aws.js';
-export const core: typeof MetatypeTypegraphCore;
-export const runtimes: typeof MetatypeTypegraphRuntimes;
-export const utils: typeof MetatypeTypegraphUtils;
-export const aws: typeof MetatypeTypegraphAws;
