e11bcaf824c7c7da733931ba419509fe062bc902
Natoandro <anatoandro@hotmail.com>
2024-07-01 09:52:46 +0300

    set path style to false

--- typegate/tests/utils/hooks.ts
+++ typegate/tests/utils/hooks.ts
@@ -4,7 +4,7 @@
 import { SyncConfig } from "@typegate/config.ts";
 import { createBucket, tryDeleteBucket } from "test-utils/s3.ts";
 import { connect } from "redis";
-import { S3Client } from "aws-sdk/client-s3";
+import { S3Client, S3ClientConfig } from "aws-sdk/client-s3";
 
 // TODO single source of truth
 const redisKey = "typegraph";
@@ -15,14 +15,26 @@ export async function clearSyncData(syncConfig: SyncConfig) {
   await redis.del(redisKey);
   await redis.del(redisEventKey);
 
-  const s3 = new S3Client(syncConfig.s3);
+  const s3Config: S3ClientConfig = { ...syncConfig.s3 };
+  if (!s3Config.forcePathStyle) {
+    const endpoint = new URL(s3Config.endpoint as string);
+    endpoint.pathname = `/${syncConfig.s3Bucket}`;
+    s3Config.endpoint = endpoint.href;
+  }
+  const s3 = new S3Client(s3Config);
   await tryDeleteBucket(s3, syncConfig.s3Bucket);
   s3.destroy();
   await redis.quit();
 }
 
 export async function setupSync(syncConfig: SyncConfig) {
-  const s3 = new S3Client(syncConfig.s3);
+  const s3Config = { ...syncConfig.s3 };
+  if (!s3Config.forcePathStyle) {
+    const endpoint = new URL(s3Config.endpoint as string);
+    endpoint.pathname = `/${syncConfig.s3Bucket}`;
+    s3Config.endpoint = endpoint.href;
+  }
+  const s3 = new S3Client(s3Config);
   await createBucket(s3, syncConfig.s3Bucket);
   s3.destroy();
 }
