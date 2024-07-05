ed865512bd2881cd3172f2ebc620e95742a4f794
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-07-06 00:18:55 +0300

    chore: bump to version 0.4.4 (#779)
    Prepare release of the 0.4.4 version.
    
    #### Migration notes
    
    - [ ] The change comes with new or modified tests
    - [ ] Hard-to-understand functions have explanatory comments
    - [ ] End-user documentation is updated to reflect the change

--- dev/consts.ts
+++ dev/consts.ts
@@ -9,8 +9,8 @@ export const WASMTIME_VERSION = "21.0.0";
 export const WASMTIME_PY_VERSION = "21.0.0";
 export const TYPEGRAPH_VERSION = "0.0.3";
 export const PRISMA_VERSION = "5.6.0";
-export const METATYPE_VERSION = "0.4.4-0";
-export const PUBLISHED_VERSION = "0.4.3";
+export const METATYPE_VERSION = "0.4.4";
+export const PUBLISHED_VERSION = "0.4.4";
 export const SDK_PACKAGE_NAME_TS = "@typegraph/sdk";
 export const TAGLINE =
   `Declarative API development platform. Build backend components with WASM, Typescript and Python, no matter where and how your (legacy) systems are.`;
