654a4d9d158d688ce32ba75766d4c69e32de96b5
j03-dev <24nomeniavo@gmail.com>
2024-07-19 08:36:19 +0300

    impl: kv runtime

---
+++ libs/common/src/typegraph/runtimes/kv.rs
@@ -0,0 +1,11 @@
+// Copyright Metatype OÜ, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct KvRuntimeData {
+    pub endpoint: String,
+    pub db_number: Option<u8>,
+    pub password: Option<String>,
+}
