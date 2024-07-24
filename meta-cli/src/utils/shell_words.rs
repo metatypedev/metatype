1f2b011612d68286c6c8e4909adb02a51bbe9ea7
j03-dev <24nomeniavo@gmail.com>
2024-07-24 21:58:29 +0300

    use the right license

--- meta-cli/src/utils/shell_words.rs
+++ meta-cli/src/utils/shell_words.rs
@@ -1,28 +1,15 @@
 // no-auto-license-header
 
 /*
-The MIT License (MIT)
+Copyright 2018 Tomasz Miąsko
 
-Copyright (c) 2020 Alexandre Piel
-Copyright 2018-2022 the Deno authors.
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE>
+or the MIT license <LICENSE-MIT>, at your option.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Process command line according to parsing rules of Unix shell as specified
+in [Shell Command Language in POSIX.1-2008][posix-shell].
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+[posix-shell]: http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
 */
 
 use core::fmt;
