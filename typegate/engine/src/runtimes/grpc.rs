e24694bb05bfea9eff2c8759f684b1dd9a754309
j03-dev <24nomeniavo@gmail.com>
2024-09-01 16:52:24 +0300

    remove duplicate license

--- typegate/engine/src/runtimes/grpc.rs
+++ typegate/engine/src/runtimes/grpc.rs
@@ -1,9 +1,6 @@
 // Copyright Metatype OÜ, licensed under the Elastic License 2.0.
 // SPDX-License-Identifier: Elastic-2.0
 
-// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
-// SPDX-License-Identifier: MPL-2.0
-
 use std::{cell::RefCell, ops::Deref, rc::Rc, str::FromStr, sync::Arc};
 
 use common::grpc::{
