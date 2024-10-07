e939f2fd87b5ecbe3df652b0dc361308a5f3efde
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-10-07 05:59:36 +0200

    wip: wip

--- tests/e2e/cli/dev_test.ts
+++ tests/e2e/cli/dev_test.ts
@@ -35,6 +35,7 @@ async function writeTypegraph(version: number | null, target = "migration.py") {
 Meta.test(
   {
     // FIXME: MET-622
+    only: true,
     name: "meta dev: choose to reset the database",
     gitRepo: {
       content: {
