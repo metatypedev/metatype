e546f4f3b4dbcdc09b14c36d69f0174e4660a2a1
Natoandro <anatoandro@hotmail.com>
2025-01-14 09:59:38 +0300

    fix pre-commit

--- tests/e2e/published/utils.ts
+++ tests/e2e/published/utils.ts
@@ -1,3 +1,6 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
 import { $ } from "@local/tools/deps.ts";
 import { projectDir } from "@local/tools/utils.ts";
 
