321a3b75ba521716e77b9a5528aba9c0a219b0d8
luckasRanarison <luckasranarison@gmail.com>
2025-03-06 07:16:42 +0300

    fix: pre-commit

--- tests/runtimes/prisma/invalid_query_test.ts
+++ tests/runtimes/prisma/invalid_query_test.ts
@@ -1,3 +1,6 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
 import { gql, Meta } from "../../utils/mod.ts";
 import { dropSchema, randomPGConnStr } from "test-utils/database.ts";
 
