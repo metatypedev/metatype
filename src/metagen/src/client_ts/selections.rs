metatypedev/metatype

commit feb7436fbb9d2f28ea9f71733469bfafadd8c34e

feat: expanded typegraph (#978)
<!--
Pull requests are squashed and merged using:
- their title as the commit message
- their description as the commit body

Having a good title and description is important for the users to get
readable changelog.
-->

<!-- 1. Explain WHAT the change is about -->

- [x] Add a crate for an expanded version of the typegraph where hoisted
parameters are expanded
- [x] Use the expanded typegraph in metagen
  - [x] fdk_rs
  - [x] client_rs
  - [x] fdk_ts
  - [x] client_ts
  - [x] fdk_py
  - [x] client_py
  - [x] fdk_substantial
- [x] Support for injection in metagen: hide injected types in input
types

<!-- 2. Explain WHY the change cannot be made simpler -->

The expanded typegraph duplicates the type for different injection data.
All numeric references for types, runtimes, materializers and policies
are inlined, removing the need to always have a reference to the
typegraph object itself.

<!-- 3. Explain HOW users should update their code -->

#### Migration notes

---

- [x] The change comes with new or modified tests
- [x] Hard-to-understand functions have explanatory comments
- [ ] End-user documentation is updated to reflect the change


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

- **Chores**
- Updated dependency and workspace configurations to incorporate a new
module for improved version management.
- **New Features**
- Introduced enhanced client generation across Python, Rust, and
TypeScript with new manifest systems to better support GraphQL
integration.
- Added new methods for managing injection nodes and their associated
data, enhancing dependency injection capabilities.
- **Refactor**
- Overhauled the type schema and metadata management systems for
improved clarity, performance, and memory safety.
- Refined error handling to provide clearer diagnostics during
multi-threaded operations.
- Restructured node metadata definitions and argument types for improved

Cargo.toml
@@ -13,6 +13,7 @@ members = [
   "src/typegate/standalone",
   "src/typegate_api",
   "src/typegraph/core",
+  "src/typegraph/graph",
   "src/typegraph/schema",
   "src/utils/archive",
   "src/utils/grpc",
@@ -44,6 +45,7 @@ typegate_engine = { path = "src/typegate/engine" }
 typegate_api = { path = "src/typegate_api" }
 grpc_utils = { path = "src/utils/grpc" }
 archive_utils = { path = "src/utils/archive" }
+typegraph = { path = "src/typegraph/graph" }
 
 # cli
 clap = "=4.5.13"

src/metagen/src/client_ts/selections.rs
@@ -3,80 +3,94 @@
 
 use std::fmt::Write;
 
-use tg_schema::*;
+use typegraph::TypeNodeExt as _;
 
+use super::shared::manifest::{ManifestEntry, ManifestPage};
 use super::utils::*;
-use crate::{interlude::*, shared::client::*, shared::types::*};
+use crate::{interlude::*, shared::client::*};
 
-pub struct TsNodeSelectionsRenderer {
-    pub arg_ty_names: Rc<NameMemo>,
+#[derive(Debug)]
+pub enum TsSelection {
+    Object(Object),
+    Union(Union),
 }
 
-impl TsNodeSelectionsRenderer {
-    /// `props` is a map of prop_name -> (SelectionType, ArgumentType)
-    fn render_for_object(
-        &self,
-        dest: &mut impl Write,
-        ty_name: &str,
-        props: IndexMap<String, SelectionTy>,
-    ) -> std::fmt::Result {
-        writeln!(
-            dest,
-            "export type {ty_name} = {{
-  _?: SelectionFlags;"
-        )?;
-        for (name, select_ty) in props {
+#[derive(Debug)]
+pub struct Object {
+    name: String,
+    props: Vec<(String, SelectionTy)>,
+}
+
+impl Object {
+    fn render(&self, dest: &mut impl Write, page: &ManifestPage<TsSelection>) -> std::fmt::Result {
+        writeln!(dest, "export type {} = {{", self.name)?;
+        for (name, select_ty) in &self.props {
             use SelectionTy::*;
             match select_ty {
                 Scalar => writeln!(dest, r#"  {name}?: ScalarSelectNoArgs;"#)?,
                 ScalarArgs { arg_ty } => {
+                    let arg_ty = page.get_ref(arg_ty).unwrap();
                     writeln!(dest, r#"  {name}?: ScalarSelectArgs<{arg_ty}>;"#)?
                 }
                 Composite { select_ty } => {
+                    let select_ty = page.get_ref(select_ty).unwrap();
                     writeln!(dest, r#"  {name}?: CompositeSelectNoArgs<{select_ty}>;"#)?
                 }
-                CompositeArgs { arg_ty, select_ty } => writeln!(
-                    dest,
-                    r#"  {name}?: CompositeSelectArgs<{arg_ty}, {select_ty}>;"#
-                )?,
+                CompositeArgs { arg_ty, select_ty } => {
+                    let arg_ty = page.get_ref(arg_ty).unwrap();
+                    let select_ty = page.get_ref(select_ty).unwrap();
+                    writeln!(
+                        dest,
+                        r#"  {name}?: CompositeSelectArgs<{arg_ty}, {select_ty}>;"#
+                    )?
+                }
             };
         }
         writeln!(dest, "}};")?;
         Ok(())
     }
+}
 
-    fn render_for_union(
-        &self,
-        dest: &mut TypeRenderer,
-        ty_name: &str,
-        variants: IndexMap<String, (String, SelectionTy)>,
-    ) -> std::fmt::Result {
-        writeln!(
-            dest,
-            "export type {ty_name} = {{
-  _?: SelectionFlags;"
-        )?;
-        for (_name, (variant_ty, select_ty)) in &variants {
+#[derive(Debug)]
+struct UnionVariant {
+    ty: String,
+    select_ty: SelectionTy,
+}
+
+#[derive(Debug)]
+pub struct Union {
+    name: String,
+    variants: Vec<UnionVariant>,
+}
+
+impl Union {
+    fn render(&self, dest: &mut impl Write, page: &ManifestPage<TsSelection>) -> std::fmt::Result {
+        writeln!(dest, "export type {} = {{", self.name)?;
+        for variant in &self.variants {
             use SelectionTy::*;
-            match select_ty {
+            match &variant.select_ty {
                 Scalar | ScalarArgs { .. } => {
                     // scalars always get selected if the union node
                     // gets selected
                     unreachable!()
                 }
                 Composite { select_ty } => {
-                    // use variant_ty as key instead of normalized struct name
-                    // we want it to match the varaint name from the NodeMetas
-                    // later so no normlalization is used
+                    let select_ty = page.get_ref(select_ty).unwrap();
+                    let variant_ty = &variant.ty;
                     writeln!(
                         dest,
                         r#"  "{variant_ty}"?: CompositeSelectNoArgs<{select_ty}>;"#
                     )?
                 }
-                CompositeArgs { arg_ty, select_ty } => writeln!(
-                    dest,
-                    r#"  "{variant_ty}"?: CompositeSelectArgs<{arg_ty}, {select_ty}>;"#
-                )?,
+                CompositeArgs { arg_ty, select_ty } => {
+                    let arg_ty = page.get_ref(arg_ty).unwrap();
+                    let select_ty = page.get_ref(select_ty).unwrap();
+                    let variant_ty = &variant.ty;
+                    writeln!(
+                        dest,
+                        r#"  "{variant_ty}"?: CompositeSelectArgs<{arg_ty}, {select_ty}>;"#
+                    )?
+                }
             };
         }
         writeln!(dest, "}};")?;
@@ -84,92 +98,78 @@ impl TsNodeSelectionsRenderer {
     }
 }
 
-impl RenderType for TsNodeSelectionsRenderer {
-    fn render(
-        &self,
-        renderer: &mut TypeRenderer,
-        cursor: &mut VisitCursor,
-    ) -> anyhow::Result<String> {
-        use heck::ToPascalCase;
+impl ManifestEntry for TsSelection {
+    type Extras = ();
 
-        let name = match cursor.node.clone().deref() {
-            TypeNode::Boolean { .. }
-            | TypeNode::Float { .. }
-            | TypeNode::Integer { .. }
-            | TypeNode::String { .. }
-            | TypeNode::File { .. } => unreachable!("scalars don't get to have selections"),
-            TypeNode::Any { .. } => unimplemented!("Any type support not implemented"),
-            TypeNode::Optional {
-                data: OptionalTypeData { item, .. },
-                ..
-            }
-            | TypeNode::List {
-                data: ListTypeData { items: item, .. },
-                ..
-            }
-            | TypeNode::Function {
-                data: FunctionTypeData { output: item, .. },
-                ..
-            } => renderer
-                .render_subgraph(*item, cursor)?
-                .0
-                .unwrap()
-                .to_string(),
-            TypeNode::Object { data, base } => {
-                let props = data
-                    .properties
-                    .iter()
-                    // generate property types first
-                    .map(|(name, &dep_id)| {
-                        eyre::Ok((
-                            normalize_struct_prop_name(name),
-                            selection_for_field(dep_id, &self.arg_ty_names, renderer, cursor)?,
-                        ))
-                    })
-                    .collect::<Result<IndexMap<_, _>, _>>()?;
-                let node_name = &base.title;
-                let ty_name = normalize_type_title(node_name);
-                let ty_name = format!("{ty_name}Selections").to_pascal_case();
-                self.render_for_object(renderer, &ty_name, props)?;
-                ty_name
-            }
-            TypeNode::Either {
-                data: EitherTypeData { one_of: variants },
-                base,
-            }
-            | TypeNode::Union {
-                data: UnionTypeData { any_of: variants },
-                base,
-            } => {
-                let variants = variants
-                    .iter()
-                    .filter_map(|&inner| {
-                        if !renderer.is_composite(inner) {
-                            return None;
-                        }
-                        let ty_name = renderer.nodes[inner as usize].deref().base().title.clone();
-                        let struct_prop_name =
-                            normalize_struct_prop_name(&normalize_type_title(&ty_name[..]));
-
-                        let selection = match selection_for_field(
-                            inner,
-                            &self.arg_ty_names,
-                            renderer,
-                            cursor,
-                        ) {
-                            Ok(selection) => selection,
-                            Err(err) => return Some(Err(err)),
-                        };
-
-                        Some(eyre::Ok((struct_prop_name, (ty_name, selection))))
-                    })
-                    .collect::<Result<IndexMap<_, _>, _>>()?;
-                let ty_name = normalize_type_title(&base.title);
-                let ty_name = format!("{ty_name}Selections").to_pascal_case();
-                self.render_for_union(renderer, &ty_name, variants)?;
-                ty_name
-            }
-        };
-        Ok(name)
+    fn render(&self, dest: &mut impl Write, page: &ManifestPage<Self>) -> std::fmt::Result {
+        match self {
+            TsSelection::Object(obj) => obj.render(dest, page),
+            TsSelection::Union(union) => union.render(dest, page),
+        }
     }
+
+    fn get_reference_expr(&self, _page: &ManifestPage<Self>) -> Option<String> {
+        match self {
+            TsSelection::Object(obj) => Some(obj.name.clone()),
+            TsSelection::Union(union) => Some(union.name.clone()),
+        }
+    }
+}
+
+pub fn manifest_page(tg: &typegraph::Typegraph) -> ManifestPage<TsSelection> {
+    let mut map = IndexMap::new();
+
+    for (key, ty) in tg.output_types.iter() {
+        if !ty.is_composite() {
+            continue;
+        }
+
+        match ty {
+            Type::Boolean(_)
+            | Type::Float(_)
+            | Type::Integer(_)
+            | Type::String(_)
+            | Type::File(_) => unreachable!("scalars don't get to have selections"),
+            Type::Optional(_) | Type::List(_) | Type::Function(_) => {}
+            Type::Object(ty) => {
+                let ty_props = ty.properties();
+                let mut props = Vec::with_capacity(ty_props.len());
+                for (prop_name, prop) in ty_props {
+                    let prop_name = normalize_struct_prop_name(prop_name);
+                    let select_ty = selection_for_field(&prop.ty);
+                    props.push((prop_name, select_ty));
+                }
+                map.insert(
+                    *key,
+                    TsSelection::Object(Object {
+                        props,
+                        name: format!("{}Selections", normalize_type_title(&ty.name())),
+                    }),
+                );
+            }
+            Type::Union(ty) => {
+                let ty_variants = ty.variants();
+                let mut variants = Vec::with_capacity(ty_variants.len());
+                for variant in ty_variants {
+                    if !variant.is_composite() {
+                        continue;
+                    }
+                    let selection = selection_for_field(variant);
+                    variants.push(UnionVariant {
+                        ty: variant.title().to_string(),
+                        select_ty: selection,
+                    });
+                }
+                map.insert(
+                    *key,
+                    TsSelection::Union(Union {
+                        name: format!("{}Selections", normalize_type_title(&ty.name())),
+                        variants,
+                    }),
+                );
+            }
+        }
+    }
+
+    map.into()
 }

src/typegraph/graph/Cargo.toml
@@ -1,0 +1,16 @@
+[package]
+name = "typegraph"
+version.workspace = true
+edition.workspace = true
+
+[dependencies]
+tg_schema.workspace = true
+serde.workspace = true
+serde_json.workspace = true
+enum_dispatch.workspace = true
+indexmap.workspace = true
+paste.workspace = true
+color-eyre.workspace = true
+
+[dev-dependencies]
+insta.workspace = true

src/typegraph/graph/src/types/list.rs
@@ -1,0 +1,97 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
+use super::{Edge, EdgeKind, Type, TypeBase, TypeNode, TypeNodeExt as _, WeakType, Wrap as _};
+use crate::{
+    conv::{dedup::DuplicationKeyGenerator, interlude::*},
+    interlude::*,
+    Arc, Once,
+};
+
+#[derive(Debug)]
+pub struct ListType {
+    pub base: TypeBase,
+    pub(crate) item: Once<Type>,
+    pub min_items: Option<u32>,
+    pub max_items: Option<u32>,
+    pub unique_items: bool,
+}
+
+impl ListType {
+    pub fn item(&self) -> &Type {
+        self.item.get().expect("item type uninitialized on list")
+    }
+}
+
+impl TypeNode for Arc<ListType> {
+    fn base(&self) -> &TypeBase {
+        &self.base
+    }
+
+    fn tag(&self) -> &'static str {
+        "list"
+    }
+
+    fn children(&self) -> Vec<Type> {
+        vec![self.item().clone()]
+    }
+
+    fn edges(&self) -> Vec<Edge> {
+        vec![Edge {
+            from: WeakType::List(Arc::downgrade(self)),
+            to: self.item().clone(),
+            kind: EdgeKind::ListItem,
+        }]
+    }
+}
+
+pub(crate) fn convert_list<G: DuplicationKeyGenerator>(
+    base: TypeBase,
+    data: &tg_schema::ListTypeData,
+    rpath: RelativePath,
+) -> Box<dyn TypeConversionResult<G>> {
+    let ty = ListType {
+        base,
+        item: Default::default(),
+        min_items: data.min_items,
+        max_items: data.max_items,
+        unique_items: data.unique_items.unwrap_or(false),
+    }
+    .into();
+
+    Box::new(ListTypeConversionResult {
+        ty,
+        item_idx: data.items,
+        rpath,
+    })
+}
+
+pub struct ListTypeConversionResult {
+    ty: Arc<ListType>,
+    item_idx: u32,
+    rpath: RelativePath,
+}
+
+impl<G: DuplicationKeyGenerator> TypeConversionResult<G> for ListTypeConversionResult {
+    fn get_type(&self) -> Type {
+        self.ty.clone().wrap()
+    }
+
+    fn finalize(&mut self, conv: &mut Conversion<G>) -> Result<()> {
+        let mut item = conv.convert_type(
+            self.ty.clone().wrap().downgrade(),
+            self.item_idx,
+            self.rpath.push(PathSegment::ListItem)?,
+        )?;
+
+        self.ty.item.set(item.get_type()).map_err(|_| {
+            eyre!(
+                "OnceLock: cannot set list item more than once; key={:?}",
+                self.ty.key()
+            )
+        })?;
+
+        item.finalize(conv)?;
+        Ok(())
+    }
+}

src/typegraph/graph/src/types/object.rs
@@ -1,0 +1,164 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
+use super::{Edge, EdgeKind, Type, TypeBase, TypeNode, WeakType, Wrap as _};
+use crate::conv::dedup::DuplicationKeyGenerator;
+use crate::conv::interlude::*;
+use crate::injection::InjectionNode;
+use crate::policies::PolicySpec;
+use crate::{interlude::*, TypeNodeExt as _};
+use indexmap::IndexMap;
+
+#[derive(Debug)]
+pub struct ObjectProperty {
+    pub ty: Type,
+    pub policies: Vec<PolicySpec>,
+    pub injection: Option<Arc<InjectionNode>>,
+    pub outjection: Option<()>,
+    pub required: bool,
+    pub as_id: bool,
+}
+
+impl ObjectProperty {
+    pub fn is_injected(&self) -> bool {
+        self.injection
+            .as_ref()
+            .filter(|inj| !inj.is_empty())
+            .is_some()
+    }
+}
+
+#[derive(Debug)]
+pub struct ObjectType {
+    pub base: TypeBase,
+    pub(crate) properties: Once<IndexMap<Arc<str>, ObjectProperty>>,
+}
+
+impl ObjectType {
+    pub fn properties(&self) -> &IndexMap<Arc<str>, ObjectProperty> {
+        self.properties
+            .get()
+            .expect("object properties uninitialized")
+    }
+
+    pub fn non_empty(self: Arc<Self>) -> Option<Arc<Self>> {
+        if self.as_ref().properties().is_empty() {
+            None
+        } else {
+            Some(self)
+        }
+    }
+}
+
+impl TypeNode for Arc<ObjectType> {
+    fn base(&self) -> &TypeBase {
+        &self.base
+    }
+
+    fn tag(&self) -> &'static str {
+        "object"
+    }
+
+    fn children(&self) -> Vec<Type> {
+        self.properties().values().map(|p| p.ty.clone()).collect()
+    }
+
+    fn edges(&self) -> Vec<Edge> {
+        self.properties()
+            .iter()
+            .map(|(name, prop)| Edge {
+                from: WeakType::Object(Arc::downgrade(self)),
+                to: prop.ty.clone(),
+                kind: EdgeKind::ObjectProperty(name.clone()),
+            })
+            .collect()
+    }
+}
+
+pub(crate) fn convert_object<G: DuplicationKeyGenerator>(
+    base: crate::TypeBase,
+    data: &tg_schema::ObjectTypeData,
+    rpath: RelativePath,
+) -> Box<dyn TypeConversionResult<G>> {
+    let ty = ObjectType {
+        base,
+        properties: Default::default(),
+    }
+    .into();
+
+    Box::new(ObjectTypeConversionResult {
+        ty,
+        properties: data.properties.clone(),
+        required: data.required.clone(),
+        id: data.id.clone(),
+        rpath,
+        policies: data.policies.clone(),
+    })
+}
+
+pub struct ObjectTypeConversionResult {
+    ty: Arc<ObjectType>,
+    properties: IndexMap<String, u32>,
+    required: Vec<String>,
+    id: Vec<String>,
+    rpath: RelativePath,
+    policies: IndexMap<String, Vec<tg_schema::PolicyIndices>>,
+}
+
+impl<G: DuplicationKeyGenerator> TypeConversionResult<G> for ObjectTypeConversionResult {
+    fn get_type(&self) -> Type {
+        self.ty.clone().wrap()
+    }
+
+    fn finalize(&mut self, conv: &mut Conversion<G>) -> Result<()> {
+        let mut properties = IndexMap::with_capacity(self.properties.len());
+
+        let weak = self.ty.clone().wrap().downgrade();
+
+        let injections = &self.ty.base.injection;
+
+        for (name, &idx) in self.properties.iter() {
+            let name: Arc<str> = name.clone().into();
+            let mut res = conv.convert_type(
+                weak.clone(),
+                idx,
+                self.rpath.push(PathSegment::ObjectProp(name.clone()))?,
+            )?;
+            res.finalize(conv)?;
+            let required = self.required.iter().any(|r| r == name.as_ref());
+            let as_id = self.id.iter().any(|r| r == name.as_ref());
+
+            let injection = injections.as_ref().and_then(|inj| match inj.as_ref() {
+                InjectionNode::Parent { children } => children.get(name.as_ref()).cloned(),
+                _ => None,
+            });
+
+            let policies = self
+                .policies
+                .get(name.as_ref())
+                .map(|pp| conv.convert_policies(pp))
+                .unwrap_or_default();
+
+            properties.insert(
+                name,
+                ObjectProperty {
+                    ty: res.get_type(),
+                    policies,
+                    injection,
+                    outjection: None, // TODO
+                    required,
+                    as_id,
+                },
+            );
+        }
+
+        self.ty.properties.set(properties).map_err(|_| {
+            eyre!(
+                "OnceLock: cannot set object properties more than once; key={:?}",
+                self.ty.key()
+            )
+        })?;
+
+        Ok(())
+    }
+}

src/typegraph/graph/src/types/optional.rs
@@ -1,0 +1,94 @@
+// Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
+use super::{Edge, EdgeKind, Type, TypeBase, TypeNode, WeakType, Wrap as _};
+use crate::conv::dedup::DuplicationKeyGenerator;
+use crate::{conv::interlude::*, Arc, Once};
+use crate::{interlude::*, TypeNodeExt as _};
+
+#[derive(Debug)]
+pub struct OptionalType {
+    pub base: TypeBase,
+    pub(crate) item: Once<Type>,
+    pub default_value: Option<serde_json::Value>,
+}
+
+impl OptionalType {
+    pub fn item(&self) -> &Type {
+        self.item
+            .get()
+            .expect("item type uninitialized on optional")
+    }
+}
+
+impl TypeNode for Arc<OptionalType> {
+    fn base(&self) -> &TypeBase {
+        &self.base
+    }
+
+    fn tag(&self) -> &'static str {
+        "optional"
+    }
+
+    fn children(&self) -> Vec<Type> {
+        vec![self.item().clone()]
+    }
+
+    fn edges(&self) -> Vec<Edge> {
+        vec![Edge {
+            from: WeakType::Optional(Arc::downgrade(self)),
+            to: self.item().clone(),
+            kind: EdgeKind::OptionalItem,
+        }]
+    }
+}
+
+pub(crate) fn convert_optional<G: DuplicationKeyGenerator>(
+    base: TypeBase,
+    data: &tg_schema::OptionalTypeData,
+    rpath: &RelativePath,
+) -> Box<dyn TypeConversionResult<G>> {
+    let ty = OptionalType {
+        base,
+        item: Default::default(),
+        default_value: data.default_value.clone(),
+    }
+    .into();
+
+    Box::new(OptionalTypeConversionResult {
+        ty,
+        item_idx: data.item,
+        rpath: rpath.clone(),
+    })
+}
+
+pub struct OptionalTypeConversionResult {
+    ty: Arc<OptionalType>,
+    item_idx: u32,
+    rpath: RelativePath,
+}
+
+impl<G: DuplicationKeyGenerator> TypeConversionResult<G> for OptionalTypeConversionResult {
+    fn get_type(&self) -> Type {
+        self.ty.clone().wrap()
+    }
+
+    fn finalize(&mut self, conv: &mut Conversion<G>) -> Result<()> {
+        let mut item = conv.convert_type(
+            self.ty.clone().wrap().downgrade(),
+            self.item_idx,
+            self.rpath.push(PathSegment::OptionalItem)?,
+        )?;
+
+        item.finalize(conv)?;
+
+        self.ty.item.set(item.get_type()).map_err(|_| {
+            eyre!(
+                "OnceLock: cannot set optional item more than once; key={:?}",
+                self.ty.key()
+            )
+        })?;
+
+        Ok(())
+    }
+}

commit 97753d466fdf310d5d047b63a508489321472db0

fix: metagen bug (regression from #978

Cargo.toml
@@ -93,7 +93,7 @@ lazy_static = "1.5.0"                                # FIXME: replace with Lazy 
 crossbeam-channel = "0.5.13"
 enum_dispatch = "0.3.13"
 tap = "1.0.1"
-derive_more = { version = "1", features = ["from"] }
+derive_more = { version = "1", features = ["from", "debug"] }
 cached = "0.53.1"                                    # FIXME: replace usage with a Lazy Cell + dashmap
 garde = "0.20"
 paste = "1.0.15"

src/metagen/src/client_ts/selections.rs
@@ -13,6 +13,7 @@ use crate::{interlude::*, shared::client::*};
 pub enum TsSelection {
     Object(Object),
     Union(Union),
+    Alias(TypeKey),
 }
 
 #[derive(Debug)]
@@ -105,13 +106,15 @@ impl ManifestEntry for TsSelection {
         match self {
             TsSelection::Object(obj) => obj.render(dest, page),
             TsSelection::Union(union) => union.render(dest, page),
+            TsSelection::Alias(_) => Ok(()),
         }
     }
 
-    fn get_reference_expr(&self, _page: &ManifestPage<Self>) -> Option<String> {
+    fn get_reference_expr(&self, page: &ManifestPage<Self>) -> Option<String> {
         match self {
             TsSelection::Object(obj) => Some(obj.name.clone()),
             TsSelection::Union(union) => Some(union.name.clone()),
+            TsSelection::Alias(key) => page.get_ref(key),
         }
     }
 }
@@ -130,7 +133,13 @@ pub fn manifest_page(tg: &typegraph::Typegraph) -> ManifestPage<TsSelection> {
             | Type::Integer(_)
             | Type::String(_)
             | Type::File(_) => unreachable!("scalars don't get to have selections"),
-            Type::Optional(_) | Type::List(_) | Type::Function(_) => {}
+            Type::Optional(ty) => {
+                map.insert(*key, TsSelection::Alias(ty.item().key()));
+            }
+            Type::List(ty) => {
+                map.insert(*key, TsSelection::Alias(ty.item().key()));
+            }
+            Type::Function(_) => {} // TODO
             Type::Object(ty) => {
                 let ty_props = ty.properties();
                 let mut props = Vec::with_capacity(ty_props.len());

src/typegraph/graph/Cargo.toml
@@ -12,6 +12,7 @@ indexmap.workspace = true
 paste.workspace = true
 color-eyre.workspace = true
 tracing.workspace = true
+derive_more.workspace = true
 
 [dev-dependencies]
 insta.workspace = true

src/typegraph/graph/src/types/list.rs
@@ -6,9 +6,10 @@ use crate::conv::dedup::{DupKey, DupKeyGen};
 use crate::conv::key::TypeKeyEx;
 use crate::{interlude::*, Arc, Once};
 
-#[derive(Debug)]
+#[derive(derive_more::Debug)]
 pub struct ListType {
     pub base: TypeBase,
+    #[debug("{:?}", item.get().map(|ty| ty.tag()))]
     pub(crate) item: Once<Type>,
     pub min_items: Option<u32>,
     pub max_items: Option<u32>,

src/typegraph/graph/src/types/object.rs
@@ -9,8 +9,9 @@ use crate::policies::PolicySpec;
 use crate::{interlude::*, TypeNodeExt as _};
 use indexmap::IndexMap;
 
-#[derive(Debug)]
+#[derive(derive_more::Debug)]
 pub struct ObjectProperty {
+    #[debug("{}", ty.tag())]
     pub ty: Type,
     pub policies: Vec<PolicySpec>,
     pub injection: Option<Arc<InjectionNode>>,

src/typegraph/graph/src/types/optional.rs
@@ -7,9 +7,10 @@ use crate::conv::key::TypeKeyEx;
 use crate::{interlude::*, TypeNodeExt as _};
 use crate::{Arc, Once};
 
-#[derive(Debug)]
+#[derive(derive_more::Debug)]
 pub struct OptionalType {
     pub base: TypeBase,
+    #[debug("{}", item.get().map(|ty| ty.tag()).unwrap_or("<unset>"))]
     pub(crate) item: Once<Type>,
     pub default_value: Option<serde_json::Value>,
 }

src/typegraph/graph/src/types/union.rs
@@ -7,9 +7,10 @@ use crate::conv::key::TypeKeyEx;
 use crate::{interlude::*, TypeNodeExt as _};
 use crate::{Arc, Once};
 
-#[derive(Debug)]
+#[derive(derive_more::Debug)]
 pub struct UnionType {
     pub base: TypeBase,
+    #[debug("{:?}", variants.get().map(|i| i.iter().map(|v| v.tag()).collect::<Vec<_>>()))]
     pub(crate) variants: Once<Vec<Type>>,
     pub either: bool,
 }